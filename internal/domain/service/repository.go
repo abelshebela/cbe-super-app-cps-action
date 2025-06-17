4ca9dc0c54811c26526c8ad3f04c00afdc149277
yishak terefe <yishakterefe22@gmail.com>
2025-06-17 14:39:14 +0300

    ft/cps_transfer_caps

--- internal/domain/service/repository.go
+++ internal/domain/service/repository.go
@@ -6,4 +6,6 @@ type Repository interface {
 	GetAllServiceDetails(ctx context.Context) ([]*Service, error)
 	GetOneServiceDetail(ctx context.Context, id string) (Service, error)
 	UpdateOneServiceDetail(ctx context.Context, id string, update Service) error
+
+	UpdateOneSeviceDeatil(ctx context.Context, pd Service) error
 }
