5538c87b52cc5da61b97df880774d8ac045f2436
yishak terefe <yishakterefe22@gmail.com>
2025-06-18 09:49:16 +0300

    fix

--- internal/port/outbound/service.go
+++ internal/port/outbound/service.go
@@ -1,13 +1,8 @@
 package outbound
 
-import (
-	"context"
-
-	"gitlab.com/bersufekadgetachew/cbe-super-app-cps-action/internal/domain/fayda_account/entity"
-)
-
+/*
 type PortalCardRepository interface {
 	InitiateDisableFaydaAccount(ctx context.Context, req entity.CPSAction) (*entity.CPSAction, error)
 	AuthorizeFaydaAccountDisable(ctx context.Context, req entity.CPSAction) (*entity.CPSAction, error)
 	RejectFaydaAccountDisable(ctx context.Context, req entity.CPSAction) (*entity.CPSAction, error)
-}
+} */
