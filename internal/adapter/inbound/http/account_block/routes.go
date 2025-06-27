497517bf15a662d965be98a6713d6d905b15d748
azme <azmetefera07@gmail.com>
2025-06-27 22:20:22 +0300

    adding block_single _user

--- internal/adapter/inbound/http/account_block/routes.go
+++ internal/adapter/inbound/http/account_block/routes.go
@@ -10,188 +10,185 @@ import (
 )
 
 func RegisterAccountBlockRoutes(
-    r chi.Router,
+    router chi.Router,
     handler accountblock.AccountBlockHandler,
     authMiddleware middleware.AuthMiddleware,
 ) {
-    routes := []sharedhttp.Route{
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/branch/filter-single",
-            Handler: handler.FilterSingleBranches,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+    router.Route("/api/v1/cbesuperapp/cps_action/account_block", func(r chi.Router) {
+        routes := []sharedhttp.Route{
+            {
+                Method:  http.MethodPost,
+                Path:    "/branch/filter_single",
+                Handler: handler.FilterSingleBranches,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
             },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/branch/disable-single",
-            Handler: handler.DisableSingleBranch,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+            {
+                Method:  http.MethodPost,
+                Path:    "/branch/disable_single",
+                Handler: handler.DisableSingleBranch,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
             },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/branch/approve-disable-single",
-            Handler: handler.ApproveSingleBranchDisable,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
+            {
+                Method:  http.MethodPost,
+                Path:    "/branch/approve_disable_single",
+                Handler: handler.ApproveSingleBranchDisable,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
+                },
             },
-        },
+            {
+                Method:  http.MethodPost,
+                Path:    "/branch/filter_multiple",
+                Handler: handler.FilterMultipleBranches,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/branch/disable_multiple",
+                Handler: handler.DisableMultipleBranches,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/branch/approve_disable_multiple",
+                Handler: handler.ApproveBulkBranchesDisable,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/region/block",
+                Handler: handler.BlockRegion,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
+            },
+            {
+                Method:  http.MethodPut,
+                Path:    "/region/update",
+                Handler: handler.UpdateRegion,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/region/approve_block",
+                Handler: handler.ApproveRegionBlock,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodGet,
+                Path:    "/region/{id}",
+                Handler: handler.GetRegionByID,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/district/block",
+                Handler: handler.BlockDistrict,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/district/approve_block",
+                Handler: handler.ApproveBlockDistrict,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodGet,
+                Path:    "/district/{id}",
+                Handler: handler.GetDistrictByID,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/city/block",
+                Handler: handler.BlockCity,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/city/approve_block",
+                Handler: handler.ApproveBlockCity,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodGet,
+                Path:    "/city/{id}",
+                Handler: handler.GetCityByID,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/user/block",
+                Handler: handler.BlockUser,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
+                },
+            },
+            {
+                Method:  http.MethodPost,
+                Path:    "/user/approve_block",
+                Handler: handler.ApproveBlockUser,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
+                },
+            },
+            {
+                Method:  http.MethodGet,
+                Path:    "/user/{id}",
+                Handler: handler.GetUserByID,
+                Middlewares: []func(http.Handler) http.Handler{
+                    authMiddleware.AuthenticateToken,
+                    authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
+                },
+            },
+        }
 
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/branch/filter-multiple",
-            Handler: handler.FilterMultipleBranches,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
-            },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/branch/disable-multiple",
-            Handler: handler.DisableMultipleBranches,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
-            },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/branch/approve-disable-multiple",
-            Handler: handler.ApproveBulkBranchesDisable,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
-            },
-        },
-
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/region/block",
-            Handler: handler.BlockRegion,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
-            },
-        },
-        {
-            Method:  http.MethodPut,
-            Path:    "/accountblock/region/update",
-            Handler: handler.UpdateRegion,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
-            },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/region/approve-block",
-            Handler: handler.ApproveRegionBlock,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
-            },
-        },
-        {
-            Method:  http.MethodGet,
-            Path:    "/accountblock/region/{id}",
-            Handler: handler.GetRegionByID,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
-            },
-        },
-
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/district/block",
-            Handler: handler.BlockDistrict,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
-            },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/district/approve-block",
-            Handler: handler.ApproveBlockDistrict,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
-            },
-        },
-        {
-            Method:  http.MethodGet,
-            Path:    "/accountblock/district/{id}",
-            Handler: handler.GetDistrictByID,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
-            },
-        },
-
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/city/block",
-            Handler: handler.BlockCity,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
-            },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/city/approve-block",
-            Handler: handler.ApproveBlockCity,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
-            },
-        },
-        {
-            Method:  http.MethodGet,
-            Path:    "/accountblock/city/{id}",
-            Handler: handler.GetCityByID,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
-            },
-        },
-
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/user/block",
-            Handler: handler.BlockUser,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker"}),
-            },
-        },
-        {
-            Method:  http.MethodPost,
-            Path:    "/accountblock/user/approve-block",
-            Handler: handler.ApproveBlockUser,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"checker", "ifb-checker"}),
-            },
-        },
-        {
-            Method:  http.MethodGet,
-            Path:    "/accountblock/user/{id}",
-            Handler: handler.GetUserByID,
-            Middlewares: []func(http.Handler) http.Handler{
-                authMiddleware.AuthenticateToken,
-                authMiddleware.AccessControl([]string{"maker", "ifb-maker", "checker", "ifb-checker"}),
-            },
-        },
-    }
-
-    sharedhttp.RegisterRoutes(r, routes)
+        sharedhttp.RegisterRoutes(r, routes)
+    })
 }
