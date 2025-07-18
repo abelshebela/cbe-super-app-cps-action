5aaadedc31285cb6bab17dacd253e85bb67b5b81
dawit-go <dawit.abrahame@eaglelionsystems.com>
2025-07-18 22:26:52 +0300

    fix: test u

--- internal/domain/ad/entity/entity.go
+++ internal/domain/ad/entity/entity.go
@@ -97,6 +97,13 @@ const (
 	RequestDeleteAdvert  RequestAction = "DELETE_ADVERT"
 )
 
+type AdvertResponse struct {
+	Page   int       `json:"page"`
+	Advert []*Advert `json:"advert"`
+	Limit  int       `json:"limit"`
+	Total  int64     `json:"total"`
+}
+
 type CPSActionResponse struct {
 	ID                string        `json:"id,omitempty"`
 	ActionCode        string        `json:"action_code,omitempty"`
