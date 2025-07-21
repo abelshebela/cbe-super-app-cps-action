fe2c873d4363a62bc8d8b6dd420ec62c40a073ab
dawit-go <dawit.abrahame@eaglelionsystems.com>
2025-07-21 09:46:59 +0300

    ref: clean up

--- internal/application/cps_action/dispatcher.go
+++ internal/application/cps_action/dispatcher.go
@@ -29,8 +29,6 @@ func (d *Dispatcher) Authorize(ctx context.Context, cpsAction *entities.CPSActio
 
 	fmt.Println(action, "action")
 	switch {
-	// case constants.IsActionInGroup(action, "Block"):
-	// 	return d.app.AccountBlockDomain.Authorize(ctx, cpsAction)
 	case constants.IsActionInGroup(action, "Bank"):
 		return d.app.BankDomain.Authorize(ctx, cpsAction)
 
@@ -58,69 +56,6 @@ func (d *Dispatcher) Authorize(ctx context.Context, cpsAction *entities.CPSActio
 
 	case constants.IsActionInGroup(action, "AdDomain"):
 		return d.app.AdDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "BPSUser"):
-	// 	return d.app.BPSUserDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Wallet"):
-	// 	return d.app.WalletDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Validation"):
-	// 	return d.app.ValidationDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "ServiceFee"):
-	// 	return d.app.ServiceFeeDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "DailyLimit"):
-	// 	return d.app.DailyLimitDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "VAT"):
-	// 	return d.app.VATDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "AuthTier"):
-	// 	return d.app.AmountBasedAuthDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Password"):
-	// 	return d.app.PasswordPolicyDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Archive"):
-	// 	return d.app.ArchiveDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "MinimumService"):
-	// 	return d.app.MinimumServiceDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "ServiceRule"):
-	// 	return d.app.ServiceRuleDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Total"):
-	// 	return d.app.TotalDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "AccessConfig"):
-	// 	return d.app.AccessConfigDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Branch"):
-	// 	return d.app.BranchDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Business"):
-	// 	return d.app.BusinessDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Event"):
-	// 	return d.app.EventDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "EventCategory"):
-	// 	return d.app.EventCategoryDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "MiniAppMerchant"):
-	// 	return d.app.MiniAppMerchantDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "BlockTime"):
-	// 	return d.app.BlockTimeDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Budget"):
-	// 	return d.app.BudgetDomain.Authorize(ctx, cpsAction)
-
-	// case constants.IsActionInGroup(action, "Product"):
-	// 	return d.app.ProductDomain.Authorize(ctx, cpsAction)
 
 	case constants.IsActionInGroup(action, "MiniAppMerchant"):
 		return d.app.MiniAppDomain.Authorize(ctx, cpsAction)
