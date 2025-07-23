d29bfcc7da664c5b8a71bc5673dd0d8ebf8628df
Yohannes Teshome <kateyohannes@gmail.com>
2025-07-23 12:06:53 +0300

    initialize starter file

---
+++ config/mongo.go
@@ -0,0 +1,46 @@
+package config
+
+import (
+	"cbe-super-app-budget/platform/logger"
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+}
+
+func ConnectMongo(logger logger.Logger, env *VaultConfig) (*mongo.Client, *mongo.Database, error) {
+	ctx, cancle := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancle()
+
+	options := options.Client().
+		ApplyURI(env.MongoDBURI).
+		SetMaxPoolSize(100).
+		SetMinPoolSize(10).
+		SetMaxConnIdleTime(5 * time.Minute).
+		SetConnectTimeout(10 * time.Second)
+
+	client, err := mongo.Connect(ctx, options)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, nil, err
+	}
+
+	logger.Info(ctx, "Successfully Connected to MongoDB!")
+	return client, client.Database(env.MongoDBDatabase), nil
+}
+
+func DisconnectMongo(ctx context.Context, client *mongo.Client, logger logger.Logger) {
+	if err := client.Disconnect(ctx); err != nil {
+		logger.Error(ctx, "Error disconnecting MongoDB", zap.Error(err))
+	} else {
+		logger.Info(ctx, "Disconnected MongoDB successfully")
+	}
+}
---
+++ platform/logger/logger.go
@@ -0,0 +1,141 @@
+package logger
+
+import (
+	"cbe-super-app-budget/internal/constants"
+	"context"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+)
+
+type Logger interface {
+	GetZapLooger() *zap.Logger
+	Named(s string) *logger
+	With(fields ...zap.Field) *logger
+	Debug(ctx context.Context, msg string, fields ...zap.Field)
+	Info(ctx context.Context, msg string, fields ...zap.Field)
+	Warn(ctx context.Context, msg string, fields ...zap.Field)
+	Error(ctx context.Context, msg string, fields ...zap.Field)
+	Panic(ctx context.Context, msg string, fields ...zap.Field)
+	Fatal(ctx context.Context, msg string, fields ...zap.Field)
+	extact(ctx context.Context) []zap.Field
+}
+
+type logger struct {
+	logger  *zap.Logger
+	options Options
+}
+
+// GetZapLooger implements Logger.
+func (l *logger) GetZapLooger() *zap.Logger {
+	return l.logger
+}
+
+// Named implements Logger.
+func (l *logger) Named(s string) *logger {
+	l2 := l.logger.Named(s)
+	return &logger{
+		logger:  l2,
+		options: l.options,
+	}
+}
+
+func (l *logger) With(fields ...zap.Field) *logger {
+	l2 := l.logger.With(fields...)
+	return &logger{
+		logger:  l2,
+		options: l.options,
+	}
+}
+
+func (l *logger) extact(ctx context.Context) []zap.Field {
+	var fields []zap.Field
+	fields = append(fields, zap.String("time", time.Now().Format(time.RFC3339)))
+
+	if ctx != nil {
+		for _, field := range l.options.ExtractFields {
+			if v := ctx.Value(field.KeyInContext); v != nil {
+				fields = append(fields, field.Func(v))
+			}
+		}
+	}
+
+	return fields
+}
+
+func (l *logger) Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	l.logger.With(l.extact(ctx)...).Debug(msg, fields...)
+}
+
+func (l *logger) Error(ctx context.Context, msg string, fields ...zap.Field) {
+	l.logger.With(l.extact(ctx)...).Error(msg, fields...)
+}
+
+func (l *logger) Fatal(ctx context.Context, msg string, fields ...zap.Field) {
+	l.logger.With(l.extact(ctx)...).Fatal(msg, fields...)
+}
+
+func (l *logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
+	l.logger.With(l.extact(ctx)...).Info(msg, fields...)
+}
+
+func (l *logger) Panic(ctx context.Context, msg string, fields ...zap.Field) {
+	l.logger.With(l.extact(ctx)...).Panic(msg, fields...)
+}
+
+func (l *logger) Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	l.logger.With(l.extact(ctx)...).Warn(msg, fields...)
+}
+
+type ExtractField struct {
+	KeyInContext any
+	Func         func(any) zap.Field
+}
+
+type Options struct {
+	ExtractFields                 []ExtractField
+	IngnoreDefacutlEEtactedFields bool
+}
+
+func New(l *zap.Logger, options Options) Logger {
+	if !options.IngnoreDefacutlEEtactedFields {
+		options.ExtractFields = append(options.ExtractFields, []ExtractField{
+			{
+				KeyInContext: middleware.RequestIDKey,
+				Func: func(v any) zap.Field {
+					if vString, ok := v.(string); ok {
+						return zap.String("x-request-id", vString)
+					}
+
+					return zap.Skip()
+				},
+			},
+			{
+				KeyInContext: constants.ContextKey("x-user-id"),
+				Func: func(v any) zap.Field {
+					if vString, ok := v.(string); ok {
+						return zap.String("x-user-id", vString)
+					}
+
+					return zap.Skip()
+				},
+			},
+			{
+				KeyInContext: constants.ContextKey("request-start-time"),
+				Func: func(v any) zap.Field {
+					if vTime, ok := v.(time.Time); ok {
+						return zap.Float64("time-since-request", float64(time.Since(vTime).Milliseconds()))
+					}
+
+					return zap.Skip()
+				},
+			},
+		}...)
+	}
+
+	return &logger{
+		logger:  l,
+		options: options,
+	}
+}
