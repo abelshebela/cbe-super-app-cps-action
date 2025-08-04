eda292e9b7634ddca51d80adb961fb5265882438
Dawit Girma <dawitgirma71@gmail.com>
2025-08-04 09:31:10 +0300

    update phase 2

--- internal/service/service.go
+++ internal/service/service.go
@@ -17,7 +17,7 @@ type SpendingService interface {
 type UserService interface {
 	DeviceLookup(ctx context.Context, req dto.DeviceLookupRequest) (*dto.DeviceLookupResponse, error)
 	PreLogin(ctx context.Context, phone string) (*dto.DeviceLookupResponse, error)
-	ChangePin(ctx context.Context, req dto.ChangePinRequest) error
+	// ChangePin(ctx context.Context, req dto.ChangePinRequest) error
 	// VerifyOtp(ctx context.Context, req dto.VerifyOTPRequest) (*dto.VerifyOtpResponse, error)
 	// ForgetPinSendOtp(ctx context.Context, phone, deviceUUID string) (*dto.ForgetPinSendOtpResponse, error)
 	// Login(ctx context.Context, req dto.LoginRequest) (*dto.LoginResponse, error)
