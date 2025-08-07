89976443bd8e4b67f9165cc0f3c48e40f7cb129b
Yohannes Teshome <yohannes.teshome@eaglelionsystems.com>
2025-08-07 18:30:52 +0300

    reinit

--- internal/domain/account/repository.go
+++
@@ -1,9 +0,0 @@
-package account
-
-import "context"
-
-type Repository interface {
-	FindAccountUserByID(ctx context.Context, id string) (*AccountUser, error)
-	UpdateUserCustomerNumber(ctx context.Context, userID, customerNumber string) error
-	CreateLinkedAccount(ctx context.Context, account *LinkedAccount) error
-}
--- pkgs/token/maker.go
+++
@@ -1,6 +0,0 @@
-package token
-
-type Maker interface {
-	CreateToken(payload *Payload) (string, error)
-	VerifyUserToken(tokenString string) (*Payload, error)
-}
