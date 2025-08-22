6054f0653d845426939868bd5de563fdd14911da
natnael-ta <natnael.tadesse@eaglelionsystems.com>
2025-08-22 09:47:04 +0300

    removed unused import

--- initiator/persistence.go
+++ initiator/persistence.go
@@ -8,7 +8,6 @@ import (
 	dal "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/infra"
 	account_block_repo "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/persistence/account_block"
 	account_validation "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/persistence/account_validation"
-	action_persistence "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/persistence/action"
 	advert "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/persistence/ad"
 	amount_based_persistence "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/persistence/amount_based_auth"
 	avatarPersitence "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/persistence/avatar"
@@ -37,7 +36,7 @@ import (
 	wallet_repo "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/adapter/outbound/persistence/wallet"
 	account_lookup_domain "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/domain/account_lookup"
 
-	"github.com/CBE-Super-App/cbe-super-app-cps-action/internal/domain/action"
+	// "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/domain/action"
 
 	"github.com/CBE-Super-App/cbe-super-app-cps-action/internal/domain/avatar"
 	cps_actions "github.com/CBE-Super-App/cbe-super-app-cps-action/internal/domain/cps_actions/repository"
