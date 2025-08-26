303cfd3ddce3086e7b6f1e3432f2ac7785e010e6
kibromeagel <kibrom.hailu@eaglelionsystems.com>
2025-08-26 15:51:46 +0300

    create the amount based auth interface

---
+++ internal/constants/interfaces/amount_based_auth/amount_based_auth.go
@@ -0,0 +1,10 @@
+package amount_based_auth 
+
+import "net/http"
+
+
+type AmountBasedAuthAdapter interface {
+	GetAllAmountBasedDetail(w http.ResponseWriter, r *http.Request)
+	UpdateAmountBasedAuth(w http.ResponseWriter, r *http.Request)
+	RejectAmountBasedAuth(w http.ResponseWriter, r *http.Request)
+}
