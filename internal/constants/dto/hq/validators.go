802821f9681a784b9c0e4d08f26bd21706c66d4a
Esubalew-Sintie <esubalewsintie1302@gmail.com>
2025-08-26 16:09:18 +0300

    ref:refactoring hq

--- internal/constants/dto/hq/validators.go
+++ internal/constants/dto/hq/validators.go
@@ -1,22 +1,38 @@
 package hqDto
 
 import (
+	"cbe-super-app-cps-action/internal/constants/localization"
+	"errors"
+
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 )
 
 func (r UpdateBlockTimeRequest) Validate() error {
-	return validation.ValidateStruct(&r,
-		validation.Field(&r.BlockTime, validation.Required, validation.Min(uint32(1)).Error("block_time must be greater than 0")),
+	err := validation.ValidateStruct(&r,
+		validation.Field(&r.BlockTime, validation.Required, validation.Min(uint32(1))),
 	)
+	if err != nil {
+		return errors.New(localization.ErrorInvalidBlockTime.Code)
+	}
+	return nil
 }
+
 func (r UpdateArchiveTimeRequest) Validate() error {
-	return validation.ValidateStruct(&r,
-		validation.Field(&r.ArchiveTime, validation.Required, validation.Min(uint32(1)).Error("archive_time must be greater than 0")),
+	err := validation.ValidateStruct(&r,
+		validation.Field(&r.ArchiveTime, validation.Required, validation.Min(uint32(1))),
 	)
+	if err != nil {
+		return errors.New(localization.ErrorInvalidArchiveTime.Code)
+	}
+	return nil
 }
 
 func (r UpdatePasswordExpiryRequest) Validate() error {
-	return validation.ValidateStruct(&r,
-		validation.Field(&r.PasswordExpiry, validation.Required, validation.Min(uint32(1)).Error("password_expiry must be greater than 0")),
+	err := validation.ValidateStruct(&r,
+		validation.Field(&r.PasswordExpiry, validation.Required, validation.Min(uint32(1))),
 	)
+	if err != nil {
+		return errors.New(localization.ErrorInvalidPasswordExpiry.Code)
+	}
+	return nil
 }
--- internal/storage/external_call/sms.go
+++ internal/storage/external_call/sms.go
@@ -1,7 +1,14 @@
 package external_call
 
 import (
+	smsDto "cbe-super-app-cps-action/internal/constants/dto/sms"
+	"cbe-super-app-cps-action/internal/constants/localization"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
 	"gitlab.com/bersufekadgetachew/cbe-super-app-shared/shared/utils"
@@ -24,45 +31,45 @@ func NewSMSPersistence(baseUrl string, logger utils.Logger) *SMSPersistence {
 	}
 }
 
-// // SendSMS sends an SMS using the external API
-// func (s *SMSPersistence) SendSMS(ctx context.Context, recipient, messageBody string) error {
+// SendSMS sends an SMS using the external API
+func (s *SMSPersistence) SendSMS(ctx context.Context, recipient, messageBody string) error {
 
-// 	payload := dto.SMSRequest{
-// 		Recipient:   recipient,
-// 		MessageBody: messageBody,
-// 	}
+	payload := smsDto.SMSRequest{
+		Recipient:   recipient,
+		MessageBody: messageBody,
+	}
 
-// 	payloadBytes, err := json.Marshal(payload)
-// 	if err != nil {
-// 		s.logger.Errorf("Failed to marshal SMS payload: %v", err)
-// 		return errors.New(localization.ErrorOTPSendFailed.Code)
-// 	}
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		s.logger.Errorf("Failed to marshal SMS payload: %v", err)
+		return errors.New(localization.ErrorOTPSendFailed.Code)
+	}
 
-// 	// Create HTTP request
-// 	req, err := http.NewRequestWithContext(ctx, "POST", s.baseURL, strings.NewReader(string(payloadBytes)))
-// 	if err != nil {
-// 		s.logger.Errorf("Failed to create HTTP request: %v", err)
-// 		return errors.New(localization.ErrorExternalServiceError.Code)
-// 	}
+	// Create HTTP request
+	req, err := http.NewRequestWithContext(ctx, "POST", s.baseURL, strings.NewReader(string(payloadBytes)))
+	if err != nil {
+		s.logger.Errorf("Failed to create HTTP request: %v", err)
+		return errors.New(localization.ErrorExternalServiceError.Code)
+	}
 
-// 	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Content-Type", "application/json")
 
-// 	// Make request
-// 	startTime := time.Now()
-// 	resp, err := s.httpClient.Do(req)
-// 	duration := time.Since(startTime)
+	// Make request
+	startTime := time.Now()
+	resp, err := s.httpClient.Do(req)
+	duration := time.Since(startTime)
 
-// 	if err != nil {
-// 		s.logger.Errorf("SMS API call failed: %v, duration: %v", err, duration)
-// 		return errors.New(localization.ErrorExternalServiceError.Code)
-// 	}
-// 	defer resp.Body.Close()
+	if err != nil {
+		s.logger.Errorf("SMS API call failed: %v, duration: %v", err, duration)
+		return errors.New(localization.ErrorExternalServiceError.Code)
+	}
+	defer resp.Body.Close()
 
-// 	return nil
-// }
+	return nil
+}
 
-// // SendOTP sends an OTP SMS with formatted message
-// func (s *SMSPersistence) SendOTP(ctx context.Context, recipient, otpCode string, validMinutes int) error {
-// 	messageBody := fmt.Sprintf("Your device lookup OTP is: %s. Valid for %d minutes.", otpCode, validMinutes)
-// 	return s.SendSMS(ctx, recipient, messageBody)
-// }
+// SendOTP sends an OTP SMS with formatted message
+func (s *SMSPersistence) SendOTP(ctx context.Context, recipient, otpCode string, validMinutes int) error {
+	messageBody := fmt.Sprintf("Your device lookup OTP is: %s. Valid for %d minutes.", otpCode, validMinutes)
+	return s.SendSMS(ctx, recipient, messageBody)
+}
