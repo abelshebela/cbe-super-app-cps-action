fec05f1f85063fc70ec68822b46ee35ba986448f
Meles-Z <meles.zawdie@gmail.com>
2025-08-29 10:25:09 +0300

    ref: mini app merchant new data on current action

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,18 +1,18 @@
 package service
 
 import (
-	"context"
-	"mime/multipart"
-    cpsuser "cbe-super-app-cps-action/internal/constants/dto/cps_user"
-	permission_dto "cbe-super-app-cps-action/internal/constants/dto/permission"
 	bank_dto "cbe-super-app-cps-action/internal/constants/dto/bank"
+	cpsuser "cbe-super-app-cps-action/internal/constants/dto/cps_user"
 	department_dto "cbe-super-app-cps-action/internal/constants/dto/department"
 	eventdto "cbe-super-app-cps-action/internal/constants/dto/event"
 	fbdto "cbe-super-app-cps-action/internal/constants/dto/feedback"
 	hqDto "cbe-super-app-cps-action/internal/constants/dto/hq"
+	permission_dto "cbe-super-app-cps-action/internal/constants/dto/permission"
 	walletDto "cbe-super-app-cps-action/internal/constants/dto/wallet"
 	"cbe-super-app-cps-action/internal/constants/model"
 	"cbe-super-app-cps-action/internal/constants/types"
+	"context"
+	"mime/multipart"
 )
 
 type CPSActionService interface {
