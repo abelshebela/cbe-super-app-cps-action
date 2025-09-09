ff1669b48e840ec169a61ae0f9012fa0d1d101a5
dawit1254 <dawit.girma@eaglelionsystems.com>
2025-09-09 10:11:09 +0300

    update chasnged

--- initiator/initiator.go
+++ initiator/initiator.go
@@ -65,10 +65,17 @@ func Init(ctx context.Context) {
 		fmt.Println("Goroutines: ", runtime.NumGoroutine())
 	}()
 	fmt.Println("Goroutines: ", runtime.NumGoroutine())
+	grpcServer := server.NewGrpcBankServer(serviceLayer.Bank, logger)
 	srv := server.NewHTTPServer(cfg, r)
+
+	// Start servers
+	go func() {
+		server.StartGrpcServer(grpcServer)
+	}()
 
 	go func() {
 		srv.HTTPServerStart(ctx, logger)
 	}()
 	srv.HTTPServerStop(ctx, logger)
+	server.StopGrpcServer(grpcServer)
 }
