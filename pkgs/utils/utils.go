1d210993a751f3d3c79b1ab3ceab2e32322ecb0b
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2025-12-12 01:38:57 +0300

    fix: remove duplicate assignment in BuildMongoFilterWithKeys function

--- pkgs/utils/utils.go
+++ pkgs/utils/utils.go
@@ -177,7 +177,6 @@ func BuildMongoFilterWithKeys(input map[string]interface{}, allowedKeys []string
 		case string:
 			if v != "" {
 				filter[key] = v
-				filter[key] = v
 			}
 		case []interface{}:
 			if len(v) > 0 {
