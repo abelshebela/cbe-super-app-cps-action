2f378dedd8cb1c5c7872bbe268be10d08cf53cd6
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2025-12-12 14:05:41 +0300

    feat(auth): integrate Redis for device ID validation in authentication middleware

--- internal/handlers/middleware/middleware.go
+++ internal/handlers/middleware/middleware.go
@@ -25,6 +25,7 @@ import (
 	cps_auth "cbe-super-app-cps-action/grpc/auth/proto"
 	"cbe-super-app-cps-action/internal/constants"
 	"cbe-super-app-cps-action/internal/constants/localization"
+	"cbe-super-app-cps-action/internal/storage"
 )
 
 func CORS() func(http.Handler) http.Handler {
@@ -72,18 +73,20 @@ type UserPayload struct {
 	Department  string   `json:"department,omitempty"`
 	NextStep    string   `json:"next_step,omitempty"`
 	Action      string   `json:"action"`
+	DeviceID    string   `json:"device_id,omitempty"`
 	SessionExp  int64    `json:"session_expiry,omitempty"`
 	Environment string   `json:"environment"`
 	Permission  []string `json:"permission_group"`
 }
 
 type authMiddleware struct {
-	client       cps_auth.CpsAuthServiceClient
-	logger       utils.Logger
-	JWTSecretKey string
-	Key          string
-	IV           string
-	cfg          config.VaultConfig
+	client          cps_auth.CpsAuthServiceClient
+	logger          utils.Logger
+	JWTSecretKey    string
+	Key             string
+	IV              string
+	cfg             config.VaultConfig
+	redisRepository storage.RedisRepository
 }
 
 type AuthMiddleware interface {
@@ -93,14 +96,15 @@ type AuthMiddleware interface {
 	RequireFormContentType() func(http.Handler) http.Handler
 }
 
-func InitAuthMiddleware(client cps_auth.CpsAuthServiceClient, secretKey, key, iv string, cfg config.VaultConfig, logger utils.Logger) AuthMiddleware {
+func InitAuthMiddleware(client cps_auth.CpsAuthServiceClient, redisRepository storage.RedisRepository, secretKey, key, iv string, cfg config.VaultConfig, logger utils.Logger) AuthMiddleware {
 	return &authMiddleware{
-		client:       client,
-		JWTSecretKey: secretKey,
-		Key:          key,
-		IV:           iv,
-		cfg:          cfg,
-		logger:       logger,
+		client:          client,
+		redisRepository: redisRepository,
+		JWTSecretKey:    secretKey,
+		Key:             key,
+		IV:              iv,
+		cfg:             cfg,
+		logger:          logger,
 	}
 }
 
@@ -223,7 +227,15 @@ func (a *authMiddleware) AuthenticateToken(next http.Handler) http.Handler {
 		} else {
 			remainTime *= 60
 		}
-		if userPayload.SessionExp != 0 {
+		deviceID, err := a.redisRepository.Get(r.Context(), fmt.Sprintf("cps:auth:device:%s", userPayload.UserID))
+		if err != nil {
+			a.logger.Warnf("failed to get device id from redis: %v", err)
+			localization.SendUnauthorizedResponse(w, localization.ErrorSessionExpired.Message)
+			return
+		}
+
+		deviceID = strings.Trim(deviceID, "\"")
+		if userPayload.SessionExp != 0 && userPayload.DeviceID == deviceID {
 			a.logger.Infof("session expiry found: %d current time:%d", userPayload.SessionExp, now, userPayload.SessionExp-now)
 			if userPayload.SessionExp < now {
 				a.logger.Warnf("session has expired")
@@ -254,6 +266,10 @@ func (a *authMiddleware) AuthenticateToken(next http.Handler) http.Handler {
 				}
 			}
 
+		} else {
+			a.logger.Warnf("session has expired")
+			localization.SendUnauthorizedResponse(w, localization.ErrorSessionExpired.Message)
+			return
 		}
 
 		r = r.WithContext(ctx)
