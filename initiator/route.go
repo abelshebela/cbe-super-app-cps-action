4dccd92f04b7ba370258c6c00f2a11420f88a19b
dave <dawit.girma@eaglelionsystems.com>
2025-12-24 01:03:08 +0300

    update

--- initiator/route.go
+++ initiator/route.go
@@ -219,6 +219,7 @@ func InitRoute(ctx context.Context, router *chi.Mux, handlerLayer Handler, clien
 			"/actions/{action_code}/approve",
 			"/actions/{action_code}/reject",
 		}))
+		r.Mount("/", r)
 
 		// CPS Actions
 		// r.Route("/actions", func(r chi.Router) {
@@ -227,7 +228,6 @@ func InitRoute(ctx context.Context, router *chi.Mux, handlerLayer Handler, clien
 		// 	r.Post("/{action_code}/reject", handlerLayer.CpsActionHandler.RejectCPSAction)
 		// })
 	})
-	secured.Mount("/", r)
 	// Mount
 	router.Mount("/api/v1/cbesuperapp/cps_action", secured)
 
