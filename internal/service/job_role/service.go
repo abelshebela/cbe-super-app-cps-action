0f8677489874fb2f2f3bc32bb0bcbb2d2cce6bab
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2025-12-28 01:22:42 +0300

    Fix role existence check in Update method to use the correct repository

--- internal/service/job_role/service.go
+++ internal/service/job_role/service.go
@@ -66,7 +66,7 @@ func (j *jobRoleService) Update(ctx context.Context, id string, update sharedmod
 	}
 
 	if update.Role != "" {
-		if err := core.CheckRoleExistent(ctx, update.Role, j.jobRoleRepository); err != nil {
+		if err := core.CheckRoleExistent(ctx, update.Role, j.roleRepository); err != nil {
 			j.logger.Errorf("[JobRole Service] the give role not found")
 			return err
 		}
