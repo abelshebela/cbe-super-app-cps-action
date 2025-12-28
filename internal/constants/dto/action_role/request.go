07423829e8b7d8dd8ad31e816dd00667452e2c2c
dawit1254 <dawit.girma@eaglelionsystems.com>
2025-12-28 16:52:28 +0300

    update on cps user

--- internal/constants/dto/action_role/request.go
+++ internal/constants/dto/action_role/request.go
@@ -26,15 +26,15 @@ type UpdateActionRoleRequest struct {
 }
 
 type GetActionRoleByActionCodeRes struct {
-	ID                   bson.ObjectID  `json:"_id" bson:"_id"`
-	ActionCode           string         `json:"action_code" bson:"action_code"`
-	ActionName           string         `json:"action_name" bson:"action_name"`
-	AssignedMakersRoles  []model.Role   `json:"assigned_makers_roles" bson:"assigned_makers_roles"`
-	AssignedCheckerRoles [][]model.Role `json:"assigned_checkers_roles" bson:"assigned_checkers_roles"`
-	AssignedAuditorRoles []model.Role   `json:"assigned_auditor_roles" bson:"assigned_auditor_roles"`
-	ApproverCount        int64          `json:"approver_count" bson:"approver_count"`
-	IsMakerOnly          bool           `json:"is_maker_only" bson:"is_maker_only"`
-	Enabled              bool           `json:"enabled" bson:"enabled"`
-	UpdatedAt            time.Time      `json:"updated_at" bson:"updated_at"`
-	CreatedAt            time.Time      `json:"created_at" bson:"created_at"`
+	ID                   bson.ObjectID     `json:"_id" bson:"_id"`
+	ActionCode           string            `json:"action_code" bson:"action_code"`
+	ActionName           string            `json:"action_name" bson:"action_name"`
+	AssignedMakersRoles  []model.JobRole   `json:"assigned_makers_roles" bson:"assigned_makers_roles"`
+	AssignedCheckerRoles [][]model.JobRole `json:"assigned_checkers_roles" bson:"assigned_checkers_roles"`
+	AssignedAuditorRoles []model.JobRole   `json:"assigned_auditor_roles" bson:"assigned_auditor_roles"`
+	ApproverCount        int64             `json:"approver_count" bson:"approver_count"`
+	IsMakerOnly          bool              `json:"is_maker_only" bson:"is_maker_only"`
+	Enabled              bool              `json:"enabled" bson:"enabled"`
+	UpdatedAt            time.Time         `json:"updated_at" bson:"updated_at"`
+	CreatedAt            time.Time         `json:"created_at" bson:"created_at"`
 }
