d54a9a2819fd918a133feb38750cbc878d7f371e
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-01-02 10:52:26 +0300

    Update ERP update function to include merchant ID and adjust request endpoint

--- internal/service/logistics_merchant/logistics_merchant.go
+++ internal/service/logistics_merchant/logistics_merchant.go
@@ -65,13 +65,13 @@ func (e *LogisticsMerchantService) Authorize(ctx context.Context, cpsAction *mod
 			))
 			return nil, err
 		}
-		if err := core.UpdateERP(ctx, e.cfg, merchant.BankAccountNumber, e.logger); err != nil {
+		if err := core.UpdateERP(ctx, e.cfg, merchant.BankAccountNumber, merchant.MerchantID, e.logger); err != nil {
 			e.logger.Errorf("Failed to update ERP after creating logistics merchant: %v", err)
 			span.AddEvent("Failed to update ERP", trace.WithAttributes(
 				attribute.String("error", err.Error()),
 				attribute.String("unique_id", cpsAction.UniqueId),
 			))
-			return nil, err
+			// return nil, err
 		}
 
 	case string(constants.RequestUpdateLogisticsMerchant):
@@ -93,13 +93,13 @@ func (e *LogisticsMerchantService) Authorize(ctx context.Context, cpsAction *mod
 			return nil, errors.New(localization.ErrorServiceUnhandledServerError.Code)
 		}
 		if prevMerchant.BankAccountNumber != merchant.BankAccountNumber {
-			if err := core.UpdateERP(ctx, e.cfg, merchant.BankAccountNumber, e.logger); err != nil {
+			if err := core.UpdateERP(ctx, e.cfg, merchant.BankAccountNumber, merchant.MerchantID, e.logger); err != nil {
 				e.logger.Errorf("Failed to update ERP after creating logistics merchant: %v", err)
 				span.AddEvent("Failed to update ERP", trace.WithAttributes(
 					attribute.String("error", err.Error()),
 					attribute.String("unique_id", cpsAction.UniqueId),
 				))
-				return nil, err
+				// return nil, err
 			}
 		}
 
