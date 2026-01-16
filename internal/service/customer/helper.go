ba6d629149bfd84951c36b8c44a7db283e7ed941
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-01-16 10:12:36 +0300

    feat(customer): implement FindCustomerByIDResponse and refactor GetCustomerByID method

---
+++ internal/service/customer/helper.go
@@ -0,0 +1,52 @@
+package customer
+
+import (
+	"cbe-super-app-cps-action/internal/constants/dto/customer"
+
+	"gitlab.com/bersufekadgetachew/cbe-super-app-shared/shared/entities/member"
+)
+
+func MapToDto(cus *member.User) customer.FindCustomerByIDResponse {
+	return customer.FindCustomerByIDResponse{
+		ID:                   cus.ID,
+		UserCode:             cus.UserCode,
+		FullName:             cus.FullName,
+		BranchCode:           cus.BranchCode,
+		ActivationBranchCode: cus.ActivationBranchCode,
+		PhoneNumber:          cus.PhoneNumber,
+		Language:             cus.Language,
+		Avatar:               cus.Avatar,
+		Email:                cus.Email,
+		PushToken:            cus.PushToken,
+		CustomerNumber:       cus.CustomerNumber,
+		UserCategory:         cus.UserCategory,
+		Industry:             cus.Industry,
+		Sector:               cus.Sector,
+		Username:             cus.Username,
+		Ownership:            cus.Ownership,
+		CustomerSegment:      cus.CustomerSegment,
+		BlockedReason:        cus.BlockedReason,
+		DeviceUUID:           cus.DeviceUUID,
+		AppVersion:           cus.AppVersion,
+		Gender:               cus.Gender,
+		MemberType:           cus.MemberType,
+		Platform:             cus.Platform,
+		DeviceStatus:         cus.DeviceStatus,
+		OnboardingMethod:     cus.OnboardingMethod,
+		KYCLevel:             cus.KYCLevel,
+		BlockedOn:            cus.BlockedOn,
+		IsUSSDEnabled:        cus.IsUSSDEnabled,
+		ISuperappEnabled:     cus.ISuperappEnabled,
+		LastLogin:            cus.LastLogin,
+		APPInstallationDate:  cus.APPInstallationDate,
+		Config:               cus.Config,
+		CreatedAt:            cus.CreatedAt,
+		ExpiryAt:             cus.ExpiryAt,
+		LastModifiedAt:       cus.LastModifiedAt,
+		IsBlocked:            cus.IsBlocked,
+		IsLocked:             cus.IsLocked,
+		Enabled:              cus.Enabled,
+		FirstPinSet:          cus.FirstPinSet,
+		IsActivated:          cus.IsActivated,
+	}
+}
