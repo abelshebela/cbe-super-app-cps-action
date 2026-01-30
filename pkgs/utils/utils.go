3165fbfa26ef0e67bab4ba375fcb2a08544242f4
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-01-30 11:55:06 +0300

    update phone formater

--- pkgs/utils/utils.go
+++ pkgs/utils/utils.go
@@ -318,28 +318,33 @@ func NoSpecialChars(value any) error {
 }
 
 func FormatPhoneNumber(phoneNumber string) string {
+	// phoneNumber = strings.TrimSpace(phoneNumber)
 	phoneNumber = strings.TrimSpace(phoneNumber)
 
+	// Remove all non-digit and non-plus characters
 	re := regexp.MustCompile(`[^\d\+]`)
 	phoneNumber = re.ReplaceAllString(phoneNumber, "")
 
-	if strings.HasPrefix(phoneNumber, "0") && len(phoneNumber) == 10 {
-		phoneNumber = "251" + phoneNumber[1:]
-	} else if strings.HasPrefix(phoneNumber, "+251") && len(phoneNumber) == 13 {
-		phoneNumber = strings.TrimPrefix(phoneNumber, "+")
-	} else if strings.HasPrefix(phoneNumber, "251") && len(phoneNumber) == 12 {
-		phoneNumber = phoneNumber
+	if strings.HasPrefix(phoneNumber, "+2510") {
+		phoneNumber = "+251" + phoneNumber[5:]
+	} else if strings.HasPrefix(phoneNumber, "2510") {
+		phoneNumber = "+251" + phoneNumber[4:]
+	} else if strings.HasPrefix(phoneNumber, "0") && len(phoneNumber) == 10 {
+		phoneNumber = "+251" + phoneNumber[1:]
 	} else if strings.HasPrefix(phoneNumber, "9") && len(phoneNumber) == 9 {
-		phoneNumber = "251" + phoneNumber
+		phoneNumber = "+251" + phoneNumber
 	} else if strings.HasPrefix(phoneNumber, "7") && len(phoneNumber) == 9 {
-		phoneNumber = "251" + phoneNumber
+		phoneNumber = "+251" + phoneNumber
+	} else if strings.HasPrefix(phoneNumber, "251") {
+		phoneNumber = "+" + phoneNumber
 	}
 
-	if strings.HasPrefix(phoneNumber, "251") && len(phoneNumber) == 12 {
+	if strings.HasPrefix(phoneNumber, "+251") && len(phoneNumber) == 13 {
 		return phoneNumber
 	}
 
 	return ""
+	
 }
 
 func ThreeNamesMinLength(value interface{}) error {
