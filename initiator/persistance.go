916b1171d8cbd606585c50ff7a2e6df3547861da
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-02-04 11:56:18 +0300

    special character validation updated for notification creation and fixed issues resulted from go vet

--- initiator/persistance.go
+++ initiator/persistance.go
@@ -147,7 +147,7 @@ func InitPersistanceLayer(client *mongo.Client, dbName string, coreConfig core.C
 		CPSRoles:                          cps_roles.NewCPSRolesStorage(client, cfg, dbName, CPSRolesCollection, logger),
 		LogisticsMerchantPersistence:      logistics_merchant_repository.NewLogisticsMerchantRepository(client, cfg, dbName, LogisticsMerchantsCollection, logger),
 		CustomerKYCPersistence:            persistence_kyc.NewCustomerKYCRepository(client, cfg, dbName, FaydaKYCollection, logger),
-		UssdMerchantPersistence:           ussd_merchant_repo.NewUssdMerchant(*client, dbName, UssdMerchantCollection, cfg, logger),
+		UssdMerchantPersistence:           ussd_merchant_repo.NewUssdMerchant(client, dbName, UssdMerchantCollection, cfg, logger),
 	}
 
 	return data
