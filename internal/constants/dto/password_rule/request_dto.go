4bd37e6f218fe415b457876d251c2402e67bd69a
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-02-12 16:23:37 +0300

    feat: enhance password rule validation with new options for sequential numbers and spaces

--- internal/constants/dto/password_rule/request_dto.go
+++ internal/constants/dto/password_rule/request_dto.go
@@ -8,13 +8,15 @@ import (
 
 type PasswordRuleUpdate struct {
 	Rule struct {
-		Name           string `json:"name"`
-		MinLength      int    `json:"min_length"`
-		MaxLength      int    `json:"max_length"`
-		Numbers        *bool  `bson:"numbers" json:"numbers"`
-		CapitalLetters *bool  `json:"capital_letters"`
-		SmallLetters   *bool  `json:"small_letters"`
-		Characters     *bool  `json:"characters"`
+		Name                   string `bson:"name" json:"name"`
+		MinLength              int    `bson:"min_length" json:"min_length"`
+		MaxLength              int    `bson:"max_length" json:"max_length"`
+		Numbers                *bool  `bson:"numbers" json:"numbers"`
+		CapitalLetters         *bool  `bson:"capital_letters" json:"capital_letters"`
+		AllowSequentialNumbers *bool  `bson:"allow_sequential_numbers" json:"allow_sequential_numbers"`
+		SmallLetters           *bool  `bson:"small_letters" json:"small_letters"`
+		Characters             *bool  `bson:"characters" json:"characters"`
+		IsSpacedAllowed        *bool  `json:"is_spaced_allowed" bson:"is_spaced_allowed"`
 	} `json:"rule"`
 }
 
