e2f04f209b8372ca9dd2f2c8852f82098f0b8295
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-02-16 15:36:17 +0300

    update on role

--- internal/constants/model/job_role.go
+++ internal/constants/model/job_role.go
@@ -7,11 +7,14 @@ import (
 )
 
 type JobRole struct {
-	ID          bson.ObjectID `bson:"_id,omitempty" json:"id"`
-	Code        string        `json:"code" bson:"code"`
-	Name        string        `json:"name" bson:"name"`
-	Enable      bool          `json:"enabled" bson:"enabled"`
-	PortalCards []string      `json:"portal_cards" bson:"portal_cards"`
-	UpdatedAt   time.Time     `json:"updated_at" bson:"updated_at"`
-	CreatedAt   time.Time     `json:"created_at" bson:"created_at"`
+	ID             bson.ObjectID `bson:"_id,omitempty" json:"id"`
+	Code           string        `json:"code" bson:"code"`
+	Name           string        `json:"name" bson:"name"`
+	Enable         bool          `json:"enabled" bson:"enabled"`
+	PortalCards    []string      `json:"portal_cards" bson:"portal_cards"`
+	MakerActions   []string      `json:"maker_actions,omitempty" bson:"-"`
+	CheckerActions []string      `json:"checker_actions,omitempty" bson:"-"`
+	AuditorActions []string      `json:"auditor_actions,omitempty" bson:"-"`
+	UpdatedAt      time.Time     `json:"updated_at" bson:"updated_at"`
+	CreatedAt      time.Time     `json:"created_at" bson:"created_at"`
 }
