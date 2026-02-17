a8a74692508ee2ed9e4743a2337089d3555d0cb7
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-02-17 11:39:29 +0300

    update cps action role

--- internal/service/cps_action_role/cps_action_role_service.go
+++ internal/service/cps_action_role/cps_action_role_service.go
@@ -472,7 +472,7 @@ func (s *cpsActionRoleService) Authorize(ctx context.Context, action *model.CPSA
 			return action, nil
 		}
 
-		prev, err := local_util.JsonUnmarshal[model.CPSActionRoleResposne](action.PreviousAction)
+		prev, err := local_util.JsonUnmarshal[imodel.CPSActionRole](action.PreviousAction)
 		if err != nil {
 			span.AddEvent("failed to unmarshal action", trace.WithAttributes(attribute.String("error", err.Error())))
 			s.logger.Errorf("failed to unmarshal action: %v", err)
