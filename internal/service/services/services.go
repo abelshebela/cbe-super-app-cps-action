8f8a0e5f7918d298bf07d7624767d737e38d9048
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-02-18 09:26:01 +0300

    service_code is optional

--- internal/service/services/services.go
+++ internal/service/services/services.go
@@ -73,7 +73,7 @@ func (s *servicesService) Update(ctx context.Context, id string, req service_dto
 	}
 
 	for _, svc := range services.Data {
-		if svc.ID.Hex() != id && (svc.ServiceCode == searchCode || svc.ServiceName == searchName) {
+		if svc.ID.Hex() != id && (svc.ServiceName == searchName) {
 			return errors.New(localization.ErrorServiceExists.Code)
 		}
 	}
