12628a091d3c4caa1ae284a67b5de9e39b81af39
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-02-26 09:37:15 +0300

    update cors middleware

--- internal/handlers/middleware/middleware.go
+++ internal/handlers/middleware/middleware.go
@@ -50,13 +50,14 @@ func CORS(cfg *config.VaultConfig) func(http.Handler) http.Handler {
 		fmt.Println("allowedOrigins***************************", cfg.GoEnv)
 		allowedOrigins = []string{"0.0.0.0:3000", "https://dev-cbe-super-app-central-portal.vercel.app"}
 	default:
+		fmt.Println("allowedOrigins FAN DEFAULT***************************")
 		allowedOrigins = []string{"0.0.0.0:3000", "*"}
 		allowCredentials = false // credentials cannot be used with wildcard origin
 	}
 
 	// fmt.Println("allowedOrigins***************************", cfg.GoEnv)
 	// temporary overide
-	// allowedOrigins = []string{"0.0.0.0:3000", "https://qa-cbe-super-app-central-portal.vercel.app", "https://dev-cbe-super-app-central-portal.vercel.app", "https://staging-cbe-super-app-central-portal.vercel.app", "https://uat-cbe-super-app-central-portal.vercel.app", "https://production-cbe-super-app-central-portal.vercel.app"}
+	allowedOrigins = []string{"0.0.0.0:3000", "https://qa-cbe-super-app-central-portal.vercel.app", "https://dev-cbe-super-app-central-portal.vercel.app", "https://staging-cbe-super-app-central-portal.vercel.app", "https://uat-cbe-super-app-central-portal.vercel.app", "https://production-cbe-super-app-central-portal.vercel.app"}
 	allowCredentials = false
 	return cors.Handler(cors.Options{
 		AllowedOrigins:   allowedOrigins,
