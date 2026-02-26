acaadf433302965315f17bca42e2bbe6637fc0c3
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-02-26 17:48:44 +0300

    feat: set DateJoined for CPSUser on creation

--- internal/storage/persistance/cps_user/repository.go
+++ internal/storage/persistance/cps_user/repository.go
@@ -48,6 +48,8 @@ func NewCPSUserRepository(client *mongo.Client, redisRepository storage.RedisRep
 // Implement actual repository methods for CPS action authorization
 func (r *CPSUserStorage) Create(ctx context.Context, cpsUser *imodel.CPSUser) error {
 	// cpsUserMap := CPSUserMapper(*cpsUser)
+	current_time := time.Now()
+	cpsUser.DateJoined = &current_time
 	_, err := r.dal.InsertOne(ctx, *cpsUser)
 	if err != nil {
 		r.logger.Errorf("[CPSUserStorage][Create] failed to create CPS user: %v", err)
