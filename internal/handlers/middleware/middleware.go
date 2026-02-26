27f0e9a10d9a3ef4d1beb431117625142b64420f
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-02-26 08:48:01 +0300

    update cors change

--- internal/handlers/middleware/middleware.go
+++ internal/handlers/middleware/middleware.go
@@ -38,7 +38,7 @@ func CORS(cfg *config.VaultConfig) func(http.Handler) http.Handler {
 	case "uat":
 		allowedOrigins = []string{"https://uat-cbe-super-app-central-portal.vercel.app"}
 	case "staging":
-		allowedOrigins = []string{"https://staging-cbe-super-app-central-portal.vercel.app"}
+		allowedOrigins = []string{"0.0.0.0:3000", "https://staging-cbe-super-app-central-portal.vercel.app"}
 	case "qa":
 		allowedOrigins = []string{"0.0.0.0:3000", "https://qa-cbe-super-app-central-portal.vercel.app"}
 	case "dev":
