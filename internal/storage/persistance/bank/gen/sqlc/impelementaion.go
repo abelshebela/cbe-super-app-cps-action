44986bfe5e259c1dd0f5351afaa3a25a056a1466
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-03-19 13:31:30 +0300

    fix(bank): change UUID generation method in Create function to use SYS_GUID()

--- internal/storage/persistance/bank/gen/sqlc/impelementaion.go
+++ internal/storage/persistance/bank/gen/sqlc/impelementaion.go
@@ -21,7 +21,7 @@ func NewBankRepository(db *sql.DB, log utils.Logger) storage.BankOracleRepositor
 }
 func (q *Queries) Create(ctx context.Context, bank *imodel.BankOracle) error {
 	query := `INSERT INTO banks (id, bank_name, logo, bic_code, is_enabled, account_length, has_alpha_numeric, create_at, update_at)
-		VALUES (gen_random_uuid(), :1, :2, :3, :4, :5, :6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`
+		VALUES (SYS_GUID(), :1, :2, :3, :4, :5, :6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)`
 	_, err := q.db.ExecContext(ctx, query,
 		bank.BankName,
 		bank.Logo,
