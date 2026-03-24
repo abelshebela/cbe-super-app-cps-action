a3eb8c9f13b5f46f55df9e12777983eff0be586e
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-03-24 13:58:02 +0300

    fix: improve BIC and name uniqueness checks in UpdateOneBank method

--- internal/storage/persistance/bank/gen/sqlc/impelementaion.go
+++ internal/storage/persistance/bank/gen/sqlc/impelementaion.go
@@ -1,7 +1,6 @@
 package sqlc
 
 import (
-	"cbe-super-app-cps-action/internal/constants/lib"
 	"cbe-super-app-cps-action/internal/constants/localization"
 	imodel "cbe-super-app-cps-action/internal/constants/model"
 	"cbe-super-app-cps-action/internal/constants/types"
@@ -155,6 +154,115 @@ func (q *Queries) FindByNameOrBIC(ctx context.Context, bic, name string) (*imode
 	return &bank, nil
 }
 
+// func (q *Queries) FindAllWithPagination(ctx context.Context, filterParam types.Filter) (*types.PaginatedResponse[[]imodel.BankOracle], error) {
+// 	q.logger.Infof("[BankOracleRepository][FindAllWithPagination] called with filter: %+v", filterParam)
+// 	// Build filtering logic
+// 	var filters []string
+// 	var args []interface{}
+// 	idx := 1
+
+// 	filters = append(filters, "1=1") // always true, simplifies appending ANDs
+
+// 	if filterParam.Search != "" && filterParam.Search != "enabled" {
+// 		search := "%" + strings.ToUpper(filterParam.Search) + "%"
+// 		filters = append(filters, fmt.Sprintf("(UPPER(bank_name) LIKE :%d OR UPPER(bic_code) LIKE :%d)", idx, idx+1))
+// 		args = append(args, search, search)
+// 		idx += 2
+// 	}
+// 	if filterParam.Search == "enabled" {
+// 		filters = append(filters, "is_enabled = 1")
+// 	}
+
+// 	oracleQuery := lib.BuildOracleFilter(filterParam, map[string]string{"bank_name": "UPPER(bank_name)", "bic_code": "UPPER(bic_code)"}, []string{"bank_name", "bic_code", "is_enabled"})
+
+// 	whereClause := oracleQuery.WhereClause
+// 	args = append(args, oracleQuery.Args)
+// 	offset := oracleQuery.Offset
+// 	limit := oracleQuery.Limit
+
+// 	// whereClause := strings.Join(filters, " AND ")
+// 	q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] whereClause: %s, args: %+v", whereClause, args)
+
+// 	// Count total
+// 	countQuery := fmt.Sprintf("SELECT COUNT(*) FROM BANKS WHERE %s", whereClause)
+// 	q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] countQuery: %s", countQuery)
+// 	var total int64
+
+// 	err := q.db.QueryRowContext(ctx, countQuery, args...).Scan(&total)
+// 	if err != nil {
+// 		q.logger.Errorf("[BankOracleRepository][FindAllWithPagination] failed to count: %v", err)
+// 		return nil, err
+// 	}
+// 	q.logger.Infof("[BankOracleRepository][FindAllWithPagination] total records: %d", total)
+
+// 	if total == 0 {
+// 		meta := local_util.BuildPaginationMeta(total, filterParam.Page, filterParam.PerPage)
+// 		resp := &types.PaginatedResponse[[]imodel.BankOracle]{
+// 			Data: []imodel.BankOracle{},
+// 			Meta: meta,
+// 		}
+// 		q.logger.Infof("[BankOracleRepository][FindAllWithPagination] no records found")
+// 		return resp, nil
+// 	}
+
+// 	// offset := (filterParam.Page - 1) * filterParam.PerPage
+// 	// limit := filterParam.PerPage
+// 	// If requested offset is beyond total, return all data (no pagination)
+// 	if int64(offset) >= total {
+// 		offset = 0
+// 		limit = total
+// 	} else if int64(offset)+int64(limit) > total {
+// 		limit = total - offset
+// 	}
+// 	q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] offset: %d, limit: %d", offset, limit)
+
+// 	// Fetch paginated results
+// 	selectQuery := fmt.Sprintf(`SELECT RAWTOHEX(ID) AS id, bank_name, logo, bic_code, is_enabled, account_length, has_alpha_numeric, create_at, update_at FROM BANKS WHERE %s ORDER BY create_at DESC OFFSET :%d ROWS FETCH NEXT :%d ROWS ONLY`, whereClause, idx, idx+1)
+// 	args = append(args, offset, limit)
+// 	q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] selectQuery: %s", selectQuery)
+// 	rows, err := q.db.QueryContext(ctx, selectQuery, args...)
+// 	if err != nil {
+// 		q.logger.Errorf("[BankOracleRepository][FindAllWithPagination] failed to fetch rows: %v", err)
+// 		return nil, err
+// 	}
+// 	defer rows.Close()
+// 	banks := []imodel.BankOracle{}
+// 	q.logger.Infof("[BankOracleRepository][FindAllWithPagination] scanning rows...")
+// 	for rows.Next() {
+// 		var bank imodel.BankOracle
+// 		err := rows.Scan(
+// 			&bank.ID,
+// 			&bank.BankName,
+// 			&bank.Logo,
+// 			&bank.BICCode,
+// 			&bank.IsEnabled,
+// 			&bank.AccountLength,
+// 			&bank.HasAlphaNumeric,
+// 			&bank.CreateAt,
+// 			&bank.UpdateAt,
+// 		)
+// 		if err != nil {
+// 			q.logger.Errorf("[BankOracleRepository][FindAllWithPagination] failed to scan row: %v", err)
+// 			return nil, err
+// 		}
+// 		q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] scanned bank: %+v", bank)
+// 		banks = append(banks, bank)
+// 	}
+// 	if err := rows.Err(); err != nil {
+// 		q.logger.Errorf("[BankOracleRepository][FindAllWithPagination] rows iteration failed: %v", err)
+// 		return nil, err
+// 	}
+
+// 	meta := local_util.BuildPaginationMeta(total, filterParam.Page, filterParam.PerPage)
+// 	q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] meta: %+v", meta)
+// 	resp := &types.PaginatedResponse[[]imodel.BankOracle]{
+// 		Data: banks,
+// 		Meta: meta,
+// 	}
+// 	q.logger.Infof("[BankOracleRepository][FindAllWithPagination] success, returning response")
+// 	return resp, nil
+// }
+
 func (q *Queries) FindAllWithPagination(ctx context.Context, filterParam types.Filter) (*types.PaginatedResponse[[]imodel.BankOracle], error) {
 	q.logger.Infof("[BankOracleRepository][FindAllWithPagination] called with filter: %+v", filterParam)
 	// Build filtering logic
@@ -170,18 +278,17 @@ func (q *Queries) FindAllWithPagination(ctx context.Context, filterParam types.F
 		args = append(args, search, search)
 		idx += 2
 	}
-	if filterParam.Search == "enabled" {
-		filters = append(filters, "is_enabled = 1")
+	if val, ok := filterParam.Filters["enabled"]; ok {
+		if b, ok := val.(bool); ok {
+			if b {
+				filters = append(filters, "is_enabled = 1")
+			} else {
+				filters = append(filters, "is_enabled = 0")
+			}
+		}
 	}
 
-	oracleQuery := lib.BuildOracleFilter(filterParam, map[string]string{"bank_name": "UPPER(bank_name)", "bic_code": "UPPER(bic_code)"}, []string{"bank_name", "bic_code", "is_enabled"})
-
-	whereClause := oracleQuery.WhereClause
-	args = append(args, oracleQuery.Args)
-	offset := oracleQuery.Offset
-	limit := oracleQuery.Limit
-
-	// whereClause := strings.Join(filters, " AND ")
+	whereClause := strings.Join(filters, " AND ")
 	q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] whereClause: %s, args: %+v", whereClause, args)
 
 	// Count total
@@ -206,14 +313,14 @@ func (q *Queries) FindAllWithPagination(ctx context.Context, filterParam types.F
 		return resp, nil
 	}
 
-	// offset := (filterParam.Page - 1) * filterParam.PerPage
-	// limit := filterParam.PerPage
+	offset := (filterParam.Page - 1) * filterParam.PerPage
+	limit := filterParam.PerPage
 	// If requested offset is beyond total, return all data (no pagination)
 	if int64(offset) >= total {
 		offset = 0
-		limit = total
+		limit = int(total)
 	} else if int64(offset)+int64(limit) > total {
-		limit = total - offset
+		limit = int(total) - offset
 	}
 	q.logger.Debugf("[BankOracleRepository][FindAllWithPagination] offset: %d, limit: %d", offset, limit)
 
