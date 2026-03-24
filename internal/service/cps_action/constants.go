ee5d79b82b789d5ae11ddde35776de7c8426c9f4
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-03-24 16:22:35 +0300

    feat(budjet and department): update on success message

--- internal/service/cps_action/constants.go
+++ internal/service/cps_action/constants.go
@@ -104,6 +104,7 @@ const (
 	// RequestDepartment               RequestAction = "DEPARMTENT"
 	RequestCreateDepartment           RequestAction = "CREATE_DEPARTMENT"
 	RequestUpdateDepartment           RequestAction = "UPDATE_DEPARTMENT"
+	RequestDeleteDepartment           RequestAction = "DELETE_DEPARTMENT"
 	RequestEnableDisableDepartment    RequestAction = "ENABLE_DISABLE_DEPARTMENT"
 	RequestEnableUser                 RequestAction = "ENABLE_USER"
 	RequestDisableUser                RequestAction = "DISABLE_USER"
@@ -833,6 +834,7 @@ var RequestActionGroups = map[string][]RequestAction{
 		RequestCreateDepartment,
 		RequestUpdateDepartment,
 		RequestEnableDisableDepartment,
+		RequestDeleteDepartment,
 	},
 	"SERVICEFEE": {
 		RequestCreateServiceFee,
