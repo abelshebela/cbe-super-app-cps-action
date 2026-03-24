89f740b585a34b968b208a88b96d38efe6314609
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-03-24 11:42:36 +0300

    fix(role): update role

--- internal/storage/persistance/job_role/repository.go
+++ internal/storage/persistance/job_role/repository.go
@@ -101,13 +101,13 @@ func (s *JobRoleStorage) SoftDelete(ctx context.Context, id string) error {
 
 func (s *JobRoleStorage) FindByID(ctx context.Context, id string) (*imodel.JobRole, error) {
 	s.logger.Infof("[JobRole/FindByID] id=%s", id)
-	objID, err := bson.ObjectIDFromHex(id)
-	if err != nil {
-		s.logger.Errorf("[JobRole/FindByID] invalid object id: %v", err)
-		return nil, errors.New(localization.ErrorInvalidID.Code)
-	}
+	// objID, err := bson.ObjectIDFromHex(id)
+	// if err != nil {
+	// 	s.logger.Errorf("[JobRole/FindByID] invalid object id: %v", err)
+	// 	return nil, errors.New(localization.ErrorInvalidID.Code)
+	// }
 
-	filter := bson.M{"_id": objID}
+	filter := bson.M{"_id": id}
 
 	res, err := s.dal.FindOne(ctx, filter, nil)
 	if err != nil {
