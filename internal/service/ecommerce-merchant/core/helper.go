26bc4a8d33234e8a9f558d740eedf905073c5cdd
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-04-01 14:42:58 +0300

    error message updated for mini-app

--- internal/service/ecommerce-merchant/core/helper.go
+++ internal/service/ecommerce-merchant/core/helper.go
@@ -174,7 +174,7 @@ func CheckMerchantExists(
 
 	res, err := merchantRepo.FindOne(ctx, filter)
 	if err != nil {
-		if err.Error() == localization.ErrorMiniAppMerchantNotFound.Code || err.Error() == localization.ErrorResourceNotFound.Code {
+		if err.Error() == localization.ErrorEcommerceMerchantNotFound.Code || err.Error() == localization.ErrorResourceNotFound.Code {
 			return false, nil
 		}
 		return false, err
