b888202ea05aaea925ec3ce95c96dee28fdda04e
bisratlike-2 <bisrat.melak@eaglelionsystems.com>
2026-04-02 06:53:17 -0400

    fix/accountblock_oracle

--- initiator/service.go
+++ initiator/service.go
@@ -81,6 +81,10 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	clientOrchestrationProducer *kafka.ClientOrchestrationProducer, presignClient *s3.PresignClient, queueManager *queue.QueueManager) service.ServiceLayer {
 
 	// Initiate Service Layer
+	// Inject Oracle-based AccountBlock into AccessListSegmentation persistence
+	// (Mongo persistence is created before Oracle, so this was deferred)
+	persistence.AccessListSegmentationPersistence.SetAccountBlockRepository(oracle.AccountBlock)
+
 	// Assign variable for minio public url
 	minioPubUrl := cfg.MinioPublicEndPoint
 
@@ -99,7 +103,7 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	walletService := wallet.NewWalletService(persistence.WalletPersistence, nil, persistence.ServicesPersistence, minioClient, minioPubUrl, cfg.S3BucketName, cfg, logger)
 	bpsActionService := bps_action_service.NewBPSActionService(persistence.BPSActionRolePersistence, persistence.BpsActionPersistence, logger, bps_action_service.Dispatcher{})
 	topupService := topup.NewTopupService(persistence.TopupPersistence, nil, minioClient, minioPubUrl, cfg.S3BucketName, cfg, logger)
-	accountBlockService := accountblock.NewAccountService(persistence.AccountBlockPersistence, nil, logger)
+	accountBlockService := accountblock.NewAccountService(oracle.AccountBlock, nil, logger)
 	departmentService := department.NewDepartmentService(persistence.DepartmentPersistence, nil, persistence.PortalCardPersistence, persistence.PermissionPersistence, logger)
 	passwordRule := password.NewPasswordRuleService(persistence.PasswordRulePersistent, nil, logger)
 	hqService := hq.NewHQService(persistence.HQPersistence, nil, logger)
@@ -120,8 +124,8 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	cpsUserService := cpsusersvc.NewCPSUserService(persistence.CpsUserPersistence, persistence.RolePersistence, persistence.CPSActionApproveIndexPersistence, persistence.DepartmentPersistence, permissionService, nil, nil, logger)
 	notificationsvc := notification.InitNotificationService(persistence.NotificationPersistence, logger, nil, smsService)
 	amountBased := amount_based_auth.NewAmountBasedAuthService(persistence.AmountBasedAuthPersistence, nil, cfg, logger)
-	unlinkService := unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, persistence.AccountBlockPersistence, nil, logger)
-	bpsUserService := bpsuser.NewBPSUserService(persistence.BPSUserPersistence, persistence.RolePersistence, nil, persistence.CpsUserPersistence, persistence.AccountBlockPersistence, logger)
+	unlinkService := unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, oracle.AccountBlock, nil, logger)
+	bpsUserService := bpsuser.NewBPSUserService(persistence.BPSUserPersistence, persistence.RolePersistence, nil, persistence.CpsUserPersistence, oracle.AccountBlock, logger)
 	articleService := media.NewMediaService(persistence.ArticlePersistence, redis, logger)
 	articleCategoryService := media.NewMediaCategoryService(persistence.ArticleCategoryPersistence, logger)
 	ShortVideoService := media.NewShortVideoService(persistence.ShortVideoPersistence, redis, mediaProducer, logger)
@@ -141,7 +145,7 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	logisticsMerchantService := logistics_merchant_service.NewLogisticsMerchantService(persistence.LogisticsMerchantPersistence, nil, accountLookupAdapter, cfg, logger)
 	servicesService := services_svc.NewServicesService(persistence.ServicesPersistence, nil, logger)
 	miniAppProductCodeContainer := miniapp.NewMiniAppProductCodeService(persistence.MiniAppProductCodePersistence, logger)
-	accessListSegmentationService := access_list_segmentation_service.NewAccessListSegmentationService(persistence.AccessListSegmentationPersistence, nil, persistence.AccessListPersistence, persistence.AccountBlockPersistence, persistence.CustomerService, persistence.CPSRoles, logger)
+	accessListSegmentationService := access_list_segmentation_service.NewAccessListSegmentationService(persistence.AccessListSegmentationPersistence, nil, persistence.AccessListPersistence, oracle.AccountBlock, persistence.CustomerService, persistence.CPSRoles, logger)
 	customerSegmentationService := customer_segmentation.NewCustomerSegmentation(persistence.CustomerSegmentation, persistence.CPSRoles, nil, logger)
 	jobRoleService := job_role.NewJobRoleService(persistence.JobRolePersistence, persistence.RolePersistence, nil, *cfg, logger)
 	RoleService := roles.NewRoleService(persistence.JobRolePersistence, persistence.PortalCardPersistence, persistence.CPSActionApproveIndexPersistence, nil, *cfg, logger)
@@ -224,7 +228,7 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	bank_service = bankService.NewBankService(logger, persistence.BankPersistence, oracle.BankOracle, cpsActionService, minioClient, minioPubUrl, cfg, cfg.S3BucketName)
 	walletService = wallet.NewWalletService(persistence.WalletPersistence, cpsActionService, persistence.ServicesPersistence, minioClient, minioPubUrl, cfg.S3BucketName, cfg, logger)
 	topupService = topup.NewTopupService(persistence.TopupPersistence, cpsActionService, minioClient, minioPubUrl, cfg.S3BucketName, cfg, logger)
-	accountBlockService = accountblock.NewAccountService(persistence.AccountBlockPersistence, cpsActionService, logger)
+	accountBlockService = accountblock.NewAccountService(oracle.AccountBlock, cpsActionService, logger)
 	departmentService = department.NewDepartmentService(persistence.DepartmentPersistence, cpsActionService, persistence.PortalCardPersistence, persistence.PermissionPersistence, logger)
 	passwordRule = password.NewPasswordRuleService(persistence.PasswordRulePersistent, cpsActionService, logger)
 	hqService = hq.NewHQService(persistence.HQPersistence, cpsActionService, logger)
@@ -247,8 +251,8 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	deviceVersionService = deviceversion.NewDeviceVersionService(persistence.DeviceVersionControlPersistence, cpsActionService, clientOrchestrationProducer, logger)
 	serviceContainer.DeviceVersionContainer = deviceVersionService
 	budgetCategoryService = budgetCategorySvc.NewBudgetCategoryService(persistence.BudgetCategoryPersistence, cpsActionService, logger, minioClient, cfg.S3BucketName, cfg, minioPubUrl)
-	serviceContainer.UnlinkContainer = unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, persistence.AccountBlockPersistence, cpsActionService, logger)
-	bpsUserService = bpsuser.NewBPSUserService(persistence.BPSUserPersistence, persistence.RolePersistence, cpsActionService, persistence.CpsUserPersistence, persistence.AccountBlockPersistence, logger)
+	serviceContainer.UnlinkContainer = unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, oracle.AccountBlock, cpsActionService, logger)
+	bpsUserService = bpsuser.NewBPSUserService(persistence.BPSUserPersistence, persistence.RolePersistence, cpsActionService, persistence.CpsUserPersistence, oracle.AccountBlock, logger)
 	serviceContainer.BPSUserContainer = bpsUserService
 	serviceContainer.AdContainer = advert.NewAdvertService(persistence.AdvertRepositoryPersistence, cpsActionService, minioClient, cfg.S3BucketName, cfg, logger)
 	serviceContainer.AvatarDomian = avatar.NewAvatarService(persistence.AvatarPersistence, cpsActionService, logger, minioClient, cfg.S3BucketName, minioPubUrl, *cfg)
@@ -272,7 +276,7 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	// miniAppMerchantService = ecommerce_merchant.NewEcommerceMerchantService(persistence.EcommerceMerchantPersistence, cpsActionService, persistence.MerchantLookup, logger, accountLookupAdapter)
 	serviceContainer.MiniAppMerchantContainer = miniMerchant
 	ecommerceMerchantService = ecommerce_merchant.NewEcommerceMerchantService(persistence.EcommerceMerchantPersistence, cpsActionService, persistence.MerchantLookup, logger, accountLookupAdapter, *cfg)
-	unlinkService = unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, persistence.AccountBlockPersistence, cpsActionService, logger)
+	unlinkService = unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, oracle.AccountBlock, cpsActionService, logger)
 	serviceContainer.Unlink = unlinkService
 	articleService = media.NewMediaService(persistence.ArticlePersistence, redis, logger)
 	serviceContainer.ArticleContainer = articleService
@@ -289,7 +293,7 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 	newsCategoryService = newscategory_service.NewNewsCategoryService(persistence.NewsCategoryPersistence, cpsActionService, logger)
 	ShortVideoService = media.NewShortVideoService(persistence.ShortVideoPersistence, redis, mediaProducer, logger)
 	articleService = media.NewMediaService(persistence.ArticlePersistence, redis, logger)
-	unlinkService = unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, persistence.AccountBlockPersistence, cpsActionService, logger)
+	unlinkService = unlink.NewUnlinkService(mongoClient, persistence.UserPersistence, persistence.ArchivedUserPersistence, persistence.LinkedAccountPersistence, persistence.ArchivedLinkedAccountPersistence, oracle.AccountBlock, cpsActionService, logger)
 	bankVaultProductService = bankvault.NewBankVaultService(oracle.BankVault, cpsActionService, logger)
 	vaultCategoryService = vault_category.NewVaultCategoryService(oracle.vaultCategory, cpsActionService, logger, minioClient, minioPubUrl, cfg.S3BucketName, cfg)
 	eventMerchantService = event_merchant_service.NewEventMerchantService(persistence.EventMerchantPersistence, cpsActionService, accountLookupAdapter, persistence.MerchantLookup, cfg, logger)
@@ -298,7 +302,7 @@ func InitServiceLayer(mongoClient *mongo.Client, persistence persistance.Persist
 
 	serviceContainer.EventMerchantServiceContainer = eventMerchantService
 	serviceContainer.LogisticsMerchantServiceContainer = logisticsMerchantService
-	accessListSegmentationService = access_list_segmentation_service.NewAccessListSegmentationService(persistence.AccessListSegmentationPersistence, cpsActionService, persistence.AccessListPersistence, persistence.AccountBlockPersistence, persistence.CustomerService, persistence.CPSRoles, logger)
+	accessListSegmentationService = access_list_segmentation_service.NewAccessListSegmentationService(persistence.AccessListSegmentationPersistence, cpsActionService, persistence.AccessListPersistence, oracle.AccountBlock, persistence.CustomerService, persistence.CPSRoles, logger)
 	customerSegmentationService = customer_segmentation.NewCustomerSegmentation(persistence.CustomerSegmentation, persistence.CPSRoles, cpsActionService, logger)
 	jobRoleService = job_role.NewJobRoleService(persistence.JobRolePersistence, persistence.RolePersistence, cpsActionService, *cfg, logger)
 	RoleService = roles.NewRoleService(persistence.JobRolePersistence, persistence.PortalCardPersistence, persistence.CPSActionApproveIndexPersistence, cpsActionService, *cfg, logger)
--- internal/service/account_block/account_block.go
+++ internal/service/account_block/account_block.go
@@ -17,6 +17,7 @@ import (
 	"gitlab.com/bersufekadgetachew/cbe-super-app-shared/shared/entities/model"
 
 	account_block_dto "cbe-super-app-cps-action/internal/constants/dto/account_block"
+	imodel "cbe-super-app-cps-action/internal/constants/model"
 	local_util "cbe-super-app-cps-action/pkgs/utils"
 
 	"gitlab.com/bersufekadgetachew/cbe-super-app-shared/shared/utils"
@@ -34,7 +35,7 @@ func NewAccountService(repo storage.AccountBlockRepository, cpsService service.C
 	return &accountBlockService{repo: repo, cpsService: cpsService, logger: logger}
 }
 
-func (s *accountBlockService) GetBranchById(ctx context.Context, id string) (*model.AccountBlock, error) {
+func (s *accountBlockService) GetBranchById(ctx context.Context, id string) (*imodel.AccountBlock, error) {
 	ctx, span := local_util.TraceLogger(ctx, "service", "GetBranchById", "BlockAccount", "GetBranchById")
 	defer span.End()
 
@@ -44,27 +45,30 @@ func (s *accountBlockService) GetBranchById(ctx context.Context, id string) (*mo
 	}
 
 	if len(block) == 0 {
-		span.AddEvent("Branch not found", trace.WithAttributes(attribute.String("id", id), attribute.String("error", err.Error())))
+		span.AddEvent("Branch not found", trace.WithAttributes(attribute.String("id", id)))
 		return nil, errors.New(localization.ErrorBranchNotFound.Code)
 	}
 
 	return block[0], nil
 }
 
-func (s *accountBlockService) GetRegionById(ctx context.Context, id string) (*model.AccountBlock, error) {
+func (s *accountBlockService) GetRegionById(ctx context.Context, id string) (*imodel.AccountBlock, error) {
 	ctx, span := local_util.TraceLogger(ctx, "service", "GetRegionById", "BlockAccount", "GetRegionById")
 	defer span.End()
 
 	block, err := s.repo.GetRegionsByIds(ctx, []string{id})
+	if err != nil {
+		return nil, err
+	}
 	if len(block) == 0 {
-		span.AddEvent("Region not found", trace.WithAttributes(attribute.String("id", id), attribute.String("error", err.Error())))
+		span.AddEvent("Region not found", trace.WithAttributes(attribute.String("id", id)))
 		return nil, errors.New(localization.ErrorRegionNotFound.Code)
 	}
 
 	return block[0], nil
 }
 
-func (s *accountBlockService) GetDistrictById(ctx context.Context, id string) (*model.AccountBlock, error) {
+func (s *accountBlockService) GetDistrictById(ctx context.Context, id string) (*imodel.AccountBlock, error) {
 	ctx, span := local_util.TraceLogger(ctx, "service", "GetDistrictById", "BlockAccount", "GetDistrictById")
 	defer span.End()
 
@@ -74,14 +78,14 @@ func (s *accountBlockService) GetDistrictById(ctx context.Context, id string) (*
 	}
 
 	if len(block) == 0 {
-		span.AddEvent("District not found", trace.WithAttributes(attribute.String("id", id), attribute.String("error", err.Error())))
+		span.AddEvent("District not found", trace.WithAttributes(attribute.String("id", id)))
 		return nil, errors.New(localization.ErrorDistrictNotFound.Code)
 	}
 
 	return block[0], nil
 }
 
-func (s *accountBlockService) GetCityById(ctx context.Context, id string) (*model.AccountBlock, error) {
+func (s *accountBlockService) GetCityById(ctx context.Context, id string) (*imodel.AccountBlock, error) {
 	ctx, span := local_util.TraceLogger(ctx, "service", "GetCityById", "BlockAccount", "GetCityById")
 	defer span.End()
 
@@ -91,18 +95,18 @@ func (s *accountBlockService) GetCityById(ctx context.Context, id string) (*mode
 	}
 
 	if len(block) == 0 {
-		span.AddEvent("City not found", trace.WithAttributes(attribute.String("id", id), attribute.String("error", err.Error())))
+		span.AddEvent("City not found", trace.WithAttributes(attribute.String("id", id)))
 		return nil, errors.New(localization.ErrorCityNotFound.Code)
 	}
 
 	return block[0], nil
 }
 
-func (s *accountBlockService) GetAllBranches(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*model.AccountBlock], error) {
+func (s *accountBlockService) GetAllBranches(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*imodel.AccountBlock], error) {
 	return s.repo.FindAllBranchesWithPagination(ctx, *filterParams)
 }
 
-func (s *accountBlockService) GetAllRegions(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*model.AccountBlock], error) {
+func (s *accountBlockService) GetAllRegions(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*imodel.AccountBlock], error) {
 	ctx, span := local_util.TraceLogger(ctx, "service", "GetAllRegions", "BlockAccount", "GetAllRegions")
 	defer span.End()
 
@@ -114,11 +118,11 @@ func (s *accountBlockService) GetAllRegions(ctx context.Context, filterParams *t
 	return regions, nil
 }
 
-func (s *accountBlockService) GetAllDistricts(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*model.AccountBlock], error) {
+func (s *accountBlockService) GetAllDistricts(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*imodel.AccountBlock], error) {
 	return s.repo.FindAllDistrictsWithPagination(ctx, *filterParams)
 }
 
-func (s *accountBlockService) GetAllCities(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*model.AccountBlock], error) {
+func (s *accountBlockService) GetAllCities(ctx context.Context, filterParams *types.Filter) (*types.PaginatedResponse[[]*imodel.AccountBlock], error) {
 	return s.repo.FindAllCitiesWithPagination(ctx, *filterParams)
 }
 
@@ -146,7 +150,9 @@ func (s *accountBlockService) EnableOrDisableBranches(ctx context.Context, branc
 
 	var cityIDs []string
 	for _, branch := range branches {
-		cityIDs = append(cityIDs, branch.CityID.Hex())
+		if branch.CityID != nil {
+			cityIDs = append(cityIDs, *branch.CityID)
+		}
 	}
 
 	// Validate cities if enabling
@@ -157,11 +163,14 @@ func (s *accountBlockService) EnableOrDisableBranches(ctx context.Context, branc
 		}
 		cityMap := make(map[string]bool)
 		for _, city := range cities {
-			cityMap[city.ID.Hex()] = city.IsEnabled
+			cityMap[city.ID] = city.IsEnabled
 		}
 
 		for _, branch := range branches {
-			if isCityEnabled, exists := cityMap[branch.CityID.Hex()]; !exists || !isCityEnabled {
+			if branch.CityID == nil {
+				return errors.New(localization.ErrorCannotEnableBranch.Code)
+			}
+			if isCityEnabled, exists := cityMap[*branch.CityID]; !exists || !isCityEnabled {
 				return errors.New(localization.ErrorCannotEnableBranch.Code)
 			}
 		}
@@ -180,12 +189,12 @@ func (s *accountBlockService) EnableOrDisableBranches(ctx context.Context, branc
 		}
 
 		previousAction = append(previousAction, types.EnableDisableAction{
-			ID:      branch.ID.Hex(),
+			ID:      branch.ID,
 			Name:    branch.Name,
 			Enabled: branch.IsEnabled,
 		})
 		currentAction = append(currentAction, types.EnableDisableAction{
-			ID:      branch.ID.Hex(),
+			ID:      branch.ID,
 			Name:    branch.Name,
 			Enabled: enabled,
 			Reason: types.Reason{
@@ -198,10 +207,8 @@ func (s *accountBlockService) EnableOrDisableBranches(ctx context.Context, branc
 
 	if len(alreadyEnabled) > 0 {
 		return fmt.Errorf("some of the branches your requested to enable are already enabled")
-		// return fmt.Errorf("these branches are already enabled: %s", strings.Join(alreadyEnabled, ", "))
 	} else if len(alreadyDisabled) > 0 {
 		return fmt.Errorf("some of the branches your requested to disable are already disabled")
-		// return fmt.Errorf("these branches are already disabled: %s", strings.Join(alreadyDisabled, ", "))
 	}
 
 	var actionType constants.ActionType
@@ -244,7 +251,7 @@ func (s *accountBlockService) EnableOrDisableRegions(ctx context.Context, region
 
 	if len(regions) != len(regionIds) {
 		s.logger.Errorf("[AccBlockSvc][EnableDisableRegions] not found")
-		return errors.New(localization.ErrorOneOrMoreInvalidCodes.Code) // Corrected error code usage
+		return errors.New(localization.ErrorOneOrMoreInvalidCodes.Code)
 	}
 
 	fullname := ctx.Value(constants.ContextKey("full_name")).(string)
@@ -260,12 +267,12 @@ func (s *accountBlockService) EnableOrDisableRegions(ctx context.Context, region
 		}
 
 		previousAction = append(previousAction, types.EnableDisableAction{
-			ID:      region.ID.Hex(),
+			ID:      region.ID,
 			Name:    region.Name,
 			Enabled: region.IsEnabled,
 		})
 		currentAction = append(currentAction, types.EnableDisableAction{
-			ID:      region.ID.Hex(),
+			ID:      region.ID,
 			Name:    region.Name,
 			Enabled: enabled,
 			Reason: types.Reason{
@@ -278,10 +285,8 @@ func (s *accountBlockService) EnableOrDisableRegions(ctx context.Context, region
 
 	if len(alreadyEnabled) > 0 {
 		return fmt.Errorf("some of the regions you requested to enable are already enabled.")
-		// return fmt.Errorf("these regions are already enabled: %s", strings.Join(alreadyEnabled, ", "))
 	} else if len(alreadyDisabled) > 0 {
 		return fmt.Errorf("some of the regions you requested to disable are already disabled.")
-		// return fmt.Errorf("these regions are already disabled: %s", strings.Join(alreadyDisabled, ", "))
 	}
 
 	var actionType constants.ActionType
@@ -330,7 +335,9 @@ func (s *accountBlockService) EnableOrDisableDistricts(ctx context.Context, dist
 
 	var regionIDs []string
 	for _, district := range districts {
-		regionIDs = append(regionIDs, district.RegionID.Hex())
+		if district.RegionID != nil {
+			regionIDs = append(regionIDs, *district.RegionID)
+		}
 	}
 
 	if enabled {
@@ -340,11 +347,14 @@ func (s *accountBlockService) EnableOrDisableDistricts(ctx context.Context, dist
 		}
 		regionMap := make(map[string]bool)
 		for _, region := range regions {
-			regionMap[region.ID.Hex()] = region.IsEnabled
+			regionMap[region.ID] = region.IsEnabled
 		}
 
 		for _, district := range districts {
-			if isRegionEnabled, exists := regionMap[district.RegionID.Hex()]; !exists || !isRegionEnabled {
+			if district.RegionID == nil {
+				return errors.New(localization.ErrorCannotEnableDistrict.Code)
+			}
+			if isRegionEnabled, exists := regionMap[*district.RegionID]; !exists || !isRegionEnabled {
 				return errors.New(localization.ErrorCannotEnableDistrict.Code)
 			}
 		}
@@ -363,12 +373,12 @@ func (s *accountBlockService) EnableOrDisableDistricts(ctx context.Context, dist
 		}
 
 		previousAction = append(previousAction, types.EnableDisableAction{
-			ID:      district.ID.Hex(),
+			ID:      district.ID,
 			Name:    district.Name,
 			Enabled: district.IsEnabled,
 		})
 		currentAction = append(currentAction, types.EnableDisableAction{
-			ID:      district.ID.Hex(),
+			ID:      district.ID,
 			Name:    district.Name,
 			Enabled: enabled,
 			Reason: types.Reason{
@@ -381,10 +391,8 @@ func (s *accountBlockService) EnableOrDisableDistricts(ctx context.Context, dist
 
 	if len(alreadyEnabled) > 0 {
 		return fmt.Errorf("some of the districts you requested to enable are already enabled.")
-		// return fmt.Errorf("these districts are already enabled: %s", strings.Join(alreadyEnabled, ", "))
 	} else if len(alreadyDisabled) > 0 {
 		return fmt.Errorf("some of the districts you requested to disable are already disabled.")
-		// return fmt.Errorf("these districts are already disabled: %s", strings.Join(alreadyDisabled, ", "))
 	}
 
 	var actionType constants.ActionType
@@ -432,7 +440,9 @@ func (s *accountBlockService) EnableOrDisableCities(ctx context.Context, ids []s
 
 	var districtIDs []string
 	for _, city := range cities {
-		districtIDs = append(districtIDs, city.DistrictID.Hex())
+		if city.DistrictID != nil {
+			districtIDs = append(districtIDs, *city.DistrictID)
+		}
 	}
 
 	if enabled {
@@ -442,11 +452,14 @@ func (s *accountBlockService) EnableOrDisableCities(ctx context.Context, ids []s
 		}
 		districtMap := make(map[string]bool)
 		for _, district := range districts {
-			districtMap[district.ID.Hex()] = district.IsEnabled
+			districtMap[district.ID] = district.IsEnabled
 		}
 
 		for _, city := range cities {
-			if isDistrictEnabled, exists := districtMap[city.DistrictID.Hex()]; !exists || !isDistrictEnabled {
+			if city.DistrictID == nil {
+				return errors.New(localization.ErrorCannotEnableDistrict.Code)
+			}
+			if isDistrictEnabled, exists := districtMap[*city.DistrictID]; !exists || !isDistrictEnabled {
 				return errors.New(localization.ErrorCannotEnableDistrict.Code)
 			}
 		}
@@ -465,12 +478,12 @@ func (s *accountBlockService) EnableOrDisableCities(ctx context.Context, ids []s
 		}
 
 		previousAction = append(previousAction, types.EnableDisableAction{
-			ID:      city.ID.Hex(),
+			ID:      city.ID,
 			Name:    city.Name,
 			Enabled: city.IsEnabled,
 		})
 		currentAction = append(currentAction, types.EnableDisableAction{
-			ID:      city.ID.Hex(),
+			ID:      city.ID,
 			Name:    city.Name,
 			Enabled: enabled,
 			Reason: types.Reason{
