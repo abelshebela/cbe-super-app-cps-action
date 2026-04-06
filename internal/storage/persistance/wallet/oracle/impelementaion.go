8fed582df2f7e035960760c8fee2a955341a6b04
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-04-06 22:52:54 +0300

    update wallet

--- internal/storage/persistance/wallet/oracle/impelementaion.go
+++ internal/storage/persistance/wallet/oracle/impelementaion.go
@@ -36,7 +36,9 @@ func oracleNumToBool(n sql.NullInt64) bool {
 }
 
 func scanWalletOracleCore(
-	row interface{ Scan(dest ...interface{}) error },
+	row interface {
+		Scan(dest ...interface{}) error
+	},
 	wallet *model.WalletOracle,
 	withService bool,
 	serviceKey, serviceCode *sql.NullString,
@@ -260,7 +262,7 @@ func (q *WalletStorage) FindAllWithPaginationForGRPC(ctx context.Context, filter
 	query := fmt.Sprintf(`
 			SELECT RAWTOHEX(w.id), w.name, w.unique_code, RAWTOHEX(w.service_id), w.enabled, w.avatar, w.services_self, w.services_other, w.services_agent, w.is_deleted, w.created_at, w.last_modified_at, w.deleted_at,
 			       sk.service_key, s.service_code
-			FROM wallets w
+			FROM WALLETS w
 			LEFT JOIN services s ON w.service_id = s.id
 			LEFT JOIN access_lists sk ON sk.id = s.access_list_id
 			%s
