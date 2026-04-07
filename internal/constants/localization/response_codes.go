0dc7f7c0367a0ed5a8247af538cf01a0c6cd904a
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-04-07 17:10:41 +0300

    feat(wallet): enhance wallet validation and repository methods to include ServiceID

--- internal/constants/localization/response_codes.go
+++ internal/constants/localization/response_codes.go
@@ -457,6 +457,7 @@ var ResponseCodesList = []ResponseCode{
 	ErrorAvatarAlreadyExist,
 	ErrorWalletNameAlreadyExists,
 	ErrorWalletCodeAlreadyExists,
+	ErrorWalletServiceAlreadyExists,
 	ErrorWalletServiceIDAlreadyExists,
 	ErrorWalletWithNameOrCodeAlreadyExists,
 	ErrorWalletAlreadyDisabled,
@@ -2910,6 +2911,12 @@ var (
 		Code:       "ERROR_WALLET_WITH_CODE_ALREADY_EXISTS",
 		StatusCode: StatusBadRequest,
 		Message:    "Wallet with the given code already exists",
+		Type:       "error",
+	}
+	ErrorWalletServiceAlreadyExists = ResponseCode{
+		Code:       "ERROR_WALLET_WITH_SERVICE_ALREADY_EXISTS",
+		StatusCode: StatusBadRequest,
+		Message:    "Wallet with the given service already exists",
 		Type:       "error",
 	}
 	ErrorWalletServiceIDAlreadyExists = ResponseCode{
