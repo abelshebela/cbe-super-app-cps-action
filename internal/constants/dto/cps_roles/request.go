4c4cb80c8d8eed708cc15f566e1d60dda6caf105
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-04-22 22:34:57 +0300

    (fix): customer roles get updated based on the new schema design

--- internal/constants/dto/cps_roles/request.go
+++ internal/constants/dto/cps_roles/request.go
@@ -3,12 +3,14 @@ package cpsroles
 type CreateCPSRoleRequest struct {
 	Name        string `json:"name"`
 	RoleCode    string `json:"role_code,omitempty"`
+	Lable       string `json:"label" bson:"lable"`
 	Description string `json:"description,omitempty"`
 }
 
 type UpdateCPSRoleRequest struct {
 	Name        *string `json:"name,omitempty"`
 	RoleCode    *string `json:"role_code,omitempty"`
+	Lable       *string `json:"label" bson:"lable"`
 	Description *string `json:"description,omitempty"`
 }
 
--- internal/constants/localization/response_codes.go
+++ internal/constants/localization/response_codes.go
@@ -698,6 +698,7 @@ var ResponseCodesList = []ResponseCode{
 	ErrorCPSRoleAlreadyEnabled,
 	ErrorCPSRoleAlreadyDisabled,
 	ErrorCPSRoleCodeAlreadyExists,
+	ErrorCPSRoleLabelAlreadyExists,
 	ErrorCPSRoleNameAlreadyExists,
 
 	// OTP related error codes
@@ -6592,6 +6593,13 @@ var (
 		Code:       "ERROR_CPS_ROLE_CODE_ALREADY_EXISTS",
 		StatusCode: StatusConflict,
 		Message:    MsgCpsRoleCodeAlreadyExists,
+		Type:       "error",
+	}
+
+	ErrorCPSRoleLabelAlreadyExists = ResponseCode{
+		Code:       "ERROR_CPS_ROLE_LABEL_ALREADY_EXISTS",
+		StatusCode: StatusConflict,
+		Message:    MsgCpsRoleLabelAlreadyExists,
 		Type:       "error",
 	}
 
