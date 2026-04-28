6140e6f6b10d274d3ca3e0db7b71490d4759a312
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-04-28 16:16:06 +0300

    (fix): access list check removed

--- internal/handlers/rest/http/ussd_merchant/ussd_merchant.go
+++ internal/handlers/rest/http/ussd_merchant/ussd_merchant.go
@@ -126,7 +126,6 @@ func (u *UssdMerchantHandler) UpdateUssdMerchant(w http.ResponseWriter, r *http.
 	defer span.End()
 	log := local_util.LoggerFromCtx(ctx, u.Logger)
 	var fileHeader *multipart.FileHeader
-	var file multipart.File
 	var err error
 	md := &types.ContextMetadata{}
 	ctx = context.WithValue(ctx, constants.ContextKeyMetadata, md)
@@ -141,24 +140,17 @@ func (u *UssdMerchantHandler) UpdateUssdMerchant(w http.ResponseWriter, r *http.
 
 	var req ussd_merchant_dto.UpdateUssdMerchantRequest
 
-	// logo is optional on update; only parse/validate when the multipart file is provided
 	_, _, formFileErr := r.FormFile("logo")
 	if formFileErr == nil {
-		file, fileHeader, err = core.ParseMultipartFormFile(r, "logo", 10<<20, string(constants.CREATE), u.Logger)
+		_, fileHeader, err = core.ParseMultipartFormFile(r, "logo", 10<<20, string(constants.CREATE), u.Logger)
 		if err != nil {
 			span.RecordError(err)
 			log.Errorf("[UpdateUssdMerchantHandler] error parsing file: %v", err)
 			localization.SendErrorResponse(w, localization.ErrorBankImageMissingOrInvalid, nil, nil)
 			return
 		}
-		defer file.Close()
-		req.Logo = fileHeader
-	} else if formFileErr != http.ErrMissingFile {
-		span.RecordError(formFileErr)
-		log.Errorf("[UpdateUssdMerchantHandler] error reading form file: %v", formFileErr)
-		localization.SendErrorResponse(w, localization.ErrorBankImageMissingOrInvalid, nil, nil)
-		return
 	}
+	req.Logo = fileHeader
 	// file, fileHeader, err = core.ParseMultipartFormFile(r, "logo", 10<<20, string(constants.CREATE), u.Logger)
 	// if err != nil {
 	// 	span.RecordError(err)
@@ -170,7 +162,7 @@ func (u *UssdMerchantHandler) UpdateUssdMerchant(w http.ResponseWriter, r *http.
 
 	req.SettlementMethod = r.FormValue("settlement_method")
 	req.Name = r.FormValue("name")
-	req.PhoneNumber = local_util.FormatPhoneNumber(r.FormValue("phone_number"))
+	req.PhoneNumber = r.FormValue("phone_number")
 	req.Service = r.FormValue("service")
 	req.Email = r.FormValue("email")
 	req.AccountNumber = r.FormValue("account_number")
