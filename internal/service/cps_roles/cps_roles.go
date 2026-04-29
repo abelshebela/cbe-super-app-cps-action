0aaf6c0890ed8c4392cf9adf40c4089bc661c3a0
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-04-29 11:51:47 +0300

    fix: issue on customer_role get fixed

--- internal/service/cps_roles/cps_roles.go
+++ internal/service/cps_roles/cps_roles.go
@@ -52,10 +52,9 @@ func (r *cpsRoleService) Create(ctx context.Context, req cps_role_dto.CreateCPSR
 		RoleCode:    req.RoleCode,
 		Lable:       req.Lable,
 		Description: req.Description,
-		// AccountType: req.AccountType,
-		Enabled:   &enabled,
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
+		Enabled:     &enabled,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
 	}
 
 	cpsActionData := lib.CpsModelBuilder("", makerUser, nil, role, string(constants.RequestCreateCpsRole), constants.CREATE)
@@ -88,10 +87,6 @@ func (r *cpsRoleService) Update(ctx context.Context, id string, req cps_role_dto
 	if req.Lable != nil {
 		label = *req.Lable
 	}
-	// if req.AccountType != nil {
-	// 	accountType = *req.AccountType
-	// }
-
 	cpsRole, err := r.repo.FindByNameOrRoleCode(ctx, name, roleCode)
 	if err != nil && err.Error() != localization.ErrorResourceNotFound.Code {
 		r.logger.Errorf("[CpsRoleSvc][Update] check existing err: %v", err)
@@ -111,10 +106,6 @@ func (r *cpsRoleService) Update(ctx context.Context, id string, req cps_role_dto
 			r.logger.Warnf("[CpsRoleSvc][Update] label exists: %s", req.Lable)
 			return errors.New(localization.ErrorCPSRoleLabelAlreadyExists.Code)
 		}
-		// if cpsRole.AccountType == accountType && existing.AccountType != accountType {
-		// 	r.logger.Warnf("[CpsRoleSvc][Update] account type exists: %s", req.AccountType)
-		// 	return errors.New(localization.ErrorCPSRoleAccountTypeAlreadyExists.Code)
-		// }
 	}
 
 	updated := *existing
@@ -130,9 +121,6 @@ func (r *cpsRoleService) Update(ctx context.Context, id string, req cps_role_dto
 	if req.Lable != nil {
 		updated.Lable = *req.Lable
 	}
-	// if req.AccountType != nil {
-	// 	updated.AccountType = *req.AccountType
-	// }
 	updated.UpdatedAt = time.Now()
 
 	cpsActionData := lib.CpsModelBuilder(id, makerUser, existing, updated, string(constants.RequestUpdateCpsRole), constants.UPDATE)
