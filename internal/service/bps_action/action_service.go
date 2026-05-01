8f5857bf165bad6780fe92ae12ec50765f62ef78
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-05-01 18:25:10 +0300

    update on bps action for auditor

--- internal/service/bps_action/action_service.go
+++ internal/service/bps_action/action_service.go
@@ -204,38 +204,6 @@ func (ba *bpsActionService) AuditorMark(ctx context.Context, actionCode string, 
 		return err
 	}
 
-	// payload := bpsActionPublishPayload{ActionCode: action.ActionCode, ActionStatus: action.Status, AuditorStatus: string(auditor.AuditorMark), RoleCode: rawRoleID, UserData: checkData, Reason: auditor.AuditorReason}
-
-	// ba.logger.Infof("[BpsActionSvc][AuditorMark] payload: %+v", payload)
-	// if err := producer.PublishMessage(ctx, payload, constants.BPSAuditorMarkTopic, ba.cfg.ACIAATMBlockUnBlockUpdateCode1, "BPS_AUDITOR_MARK"); err != nil {
-
-	// 	span.AddEvent("failed to publish bps auditor mark", trace.WithAttributes(attribute.String("error", err.Error())))
-	// 	ba.logger.Errorf("[BpsActionSvc][AuditorMark] failed to publish bps auditor mark: %v", err)
-	// 	return errors.New(localization.ErrorUnexpectedError.Code)
-
-	// }
-
-	// ba.logger.Infof("[BpsActionSvc][AuditorMark] auditor mark published for action %s, mark=%s", actionCode, auditor.AuditorMark)
-
-	// if customerBar && auditor.AuditorMark == model.MARKEDASWRONG {
-	// 	userCode := action.EntityIdentifyer
-	// 	if strings.TrimSpace(userCode) == "" {
-	// 		ba.logger.Errorf("[BpsActionSvc][AuditorMark] no user_code (entity_identifier) on action: %s", actionCode)
-	// 		span.AddEvent("missing entity_identifier for customer bar")
-	// 		return errors.New(localization.ErrorInvalidInputParameter.Code)
-	// 	}
-
-	// 	if ba.customerRepo != nil {
-	// 		if err := ba.customerRepo.BlockCustomerByUserCode(ctx, userCode); err != nil {
-	// 			ba.logger.Errorf("[BpsActionSvc][AuditorMark] failed to block customer %s: %v", userCode, err)
-	// 			span.RecordError(err)
-	// 			return err
-	// 		}
-	// 		ba.logger.Infof("[BpsActionSvc][AuditorMark] customer %s blocked successfully for action %s", userCode, actionCode)
-	// 		span.AddEvent("customer blocked", trace.WithAttributes(attribute.String("user_code", userCode)))
-	// 	}
-	// }
-
 	return nil
 
 }
