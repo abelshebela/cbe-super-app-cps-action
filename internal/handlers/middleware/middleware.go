63d49d4f69f44eb3ac090e36c50114eac9c7e6e6
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-05-13 12:33:55 +0300

    update donation on action

--- internal/handlers/middleware/middleware.go
+++ internal/handlers/middleware/middleware.go
@@ -328,7 +328,7 @@ func (a *authMiddleware) AuthenticateToken(next http.Handler) http.Handler {
 
 		r = r.WithContext(ctx)
 		// Whitelist CPS Action endpoints that don't need action-based validation
-		whitelist := []string{"cps_action", "cps_actions", "account_lookup"}
+		whitelist := []string{"cps_action", "cps_actions", "actions", "bps_actions", "account_lookup"}
 		if err := CPSActionRouteGuard(r, whitelist); err != nil {
 			a.logger.Warnf("[AuthMW][AuthToken] route guard blocked access to path: %s error: %v", r.URL.Path, err)
 			localization.SendUnauthorizedResponse(w, localization.ErrorOperationNotAllowed.Message)
