0fe3685234f500f09a358fcfcf0eb647f56e1d6f
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-05-15 17:37:38 +0300

    update service fetch

--- internal/storage/persistance/services/repository_oracle.go
+++ internal/storage/persistance/services/repository_oracle.go
@@ -832,26 +832,32 @@ func (s *ServicesStorage) FindAllWithPagination(ctx context.Context, filterParam
 	}
 
 	listQ := fmt.Sprintf(`
-SELECT
-  RAWTOHEX(s.id),
-  RAWTOHEX(s.access_list_id),
-  sk.name,
-  sk.service_key,
-  s.service_code,
-  s.minimum_fraud_amount,
-  s.product_gl_account_number,
-  s.product_gl_account_currency,
-  sk.is_enabled,
-  sk.is_deleted,
-  s.created_at,
-  s.last_modified_at,
-  sk.deleted_at
-%s
-WHERE %s
-ORDER BY s.created_at DESC
-OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY`, fromClause, where)
+	SELECT
+	RAWTOHEX(s.id),
+	RAWTOHEX(s.access_list_id),
+	sk.name,
+	sk.service_key,
+	s.service_code,
+	s.minimum_fraud_amount,
+	s.product_gl_account_number,
+	s.product_gl_account_currency,
+	sk.is_enabled,
+	sk.is_deleted,
+	s.created_at,
+	s.last_modified_at,
+	sk.deleted_at
+	%s
+	WHERE %s
+	ORDER BY s.created_at DESC
+	OFFSET :offset ROWS FETCH NEXT :limit ROWS ONLY`, fromClause, where)
 
-	listArgs := append(args, sql.Named("offset", offset), sql.Named("limit", limit))
+	var listArgs []interface{}
+	if filterParam.Filters["list"] == "all" {
+		listArgs = args
+	} else {
+		listArgs = append(args, sql.Named("offset", offset), sql.Named("limit", limit))
+	}
+
 	rows, err := s.db.QueryContext(ctx, listQ, listArgs...)
 	if err != nil {
 		s.logger.Errorf("[ServicesRepo][FindAllWithPagination] list query failed: %v", err)
