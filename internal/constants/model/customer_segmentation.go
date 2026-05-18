76a85c76174d687eb0e974145c8e4bca09443483
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-05-18 16:10:28 +0300

    label added to customer segmentation, groups, and sub_segment, and response changed

--- internal/constants/model/customer_segmentation.go
+++ internal/constants/model/customer_segmentation.go
@@ -1,28 +1,139 @@
 package model
 
 import (
+	"encoding/json"
 	"time"
 )
 
 type CustomerRoleInfo struct {
-	ID   string `json:"id" bson:"_id,omitempty"`
-	Name string `json:"name" bson:"name"`
+	ID    string `json:"id" bson:"_id,omitempty"`
+	Name  string `json:"name" bson:"name"`
+	Label string `json:"label,omitempty" bson:"label,omitempty"`
 }
 
+type CustGroupBlock struct {
+	CustGroup      string `json:"cust_group"`
+	CustGroupLabel string `json:"cust_group_label,omitempty"`
+}
+
+type CustSegmentBlock struct {
+	CustSegmentName  string `json:"cust_segment_name"`
+	CustSegmentLabel string `json:"cust_segment_label,omitempty"`
+}
+
+type CustSubSegmentBlock struct {
+	CustSubSegmentName  string `json:"cust_sub_segment_name"`
+	CustSubSegmentLabel string `json:"cust_sub_segment_label,omitempty"`
+}
+
+type CustomerEntry struct {
+	Group      CustGroupBlock      `json:"group"`
+	Segment    CustSegmentBlock    `json:"segment"`
+	SubSegment CustSubSegmentBlock `json:"sub_segment"`
+}
+
+// CustSegment is the flat representation used by persistence.
 type CustSegment struct {
-	Id                 string `json:"id,omitempty" bson:"id"`
-	CustomerGroup      string `json:"cust_group" bson:"cust_group"`
-	CustomerSegment    string `json:"cust_segment" bson:"cust_segment"`
-	CustomerSubSegment string `json:"cus_sub_segment" bson:"cust_sub_segment"`
+	Id              string `json:"-" bson:"id"`
+	CustGroupName   string `json:"-" bson:"custGroupName"`
+	CustGroupLabel  string `json:"-" bson:"custGroupLabel,omitempty"`
+	CustSegName     string `json:"-" bson:"custSegName"`
+	CustSegLabel    string `json:"-" bson:"custSegLabel,omitempty"`
+	CustSubSegName  string `json:"-" bson:"custSubSegName"`
+	CustSubSegLabel string `json:"-" bson:"custSubSegLabel,omitempty"`
+}
+
+func (e CustomerEntry) ToCustSegment() CustSegment {
+	return CustSegment{
+		CustGroupName:   e.Group.CustGroup,
+		CustGroupLabel:  e.Group.CustGroupLabel,
+		CustSegName:     e.Segment.CustSegmentName,
+		CustSegLabel:    e.Segment.CustSegmentLabel,
+		CustSubSegName:  e.SubSegment.CustSubSegmentName,
+		CustSubSegLabel: e.SubSegment.CustSubSegmentLabel,
+	}
+}
+
+func CustSegmentToEntry(s CustSegment) CustomerEntry {
+	return CustomerEntry{
+		Group: CustGroupBlock{
+			CustGroup:      s.CustGroupName,
+			CustGroupLabel: s.CustGroupLabel,
+		},
+		Segment: CustSegmentBlock{
+			CustSegmentName:  s.CustSegName,
+			CustSegmentLabel: s.CustSegLabel,
+		},
+		SubSegment: CustSubSegmentBlock{
+			CustSubSegmentName:  s.CustSubSegName,
+			CustSubSegmentLabel: s.CustSubSegLabel,
+		},
+	}
+}
+
+func CustomerEntriesToCustSegments(entries []CustomerEntry) []CustSegment {
+	segments := make([]CustSegment, 0, len(entries))
+	for _, e := range entries {
+		seg := e.ToCustSegment()
+		segments = append(segments, seg)
+	}
+	return segments
+}
+
+func CustSegmentsToCustomerEntries(segments []CustSegment) []CustomerEntry {
+	entries := make([]CustomerEntry, 0, len(segments))
+	for _, s := range segments {
+		entries = append(entries, CustSegmentToEntry(s))
+	}
+	return entries
 }
 
 type CustomerSegmentation struct {
 	ID                        string           `json:"id" bson:"_id,omitempty"`
 	CustomerRole              CustomerRoleInfo `json:"customer_role" bson:"customer_role"`
 	RemovedCustomerSegmentIDs []string         `json:"removed_customer_segment_ids,omitempty"`
-	CustomerSegments          []CustSegment    `json:"t24_customer_sub_segments" bson:"t24_customer_sub_segments"`
+	Customer                  []CustomerEntry  `json:"customer"`
+	CustomerSegments          []CustSegment    `json:"-" bson:"t24_customer_sub_segments"`
 	IsEnabled                 bool             `json:"is_enabled" bson:"is_enabled"`
 	IsDeleted                 bool             `json:"is_deleted" bson:"is_deleted"`
 	CreatedAt                 time.Time        `json:"created_at" bson:"created_at"`
 	UpdatedAt                 time.Time        `json:"updated_at" bson:"updated_at"`
+}
+
+func (c *CustomerSegmentation) SyncSegmentsFromCustomer() {
+	if len(c.Customer) == 0 {
+		return
+	}
+	c.CustomerSegments = CustomerEntriesToCustSegments(c.Customer)
+}
+
+func (c *CustomerSegmentation) SyncCustomerFromSegments() {
+	if len(c.CustomerSegments) == 0 {
+		return
+	}
+	c.Customer = CustSegmentsToCustomerEntries(c.CustomerSegments)
+}
+
+func (c *CustomerSegmentation) UnmarshalJSON(data []byte) error {
+	type customerSegmentation CustomerSegmentation
+	aux := customerSegmentation{}
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+	*c = CustomerSegmentation(aux)
+	if len(c.CustomerSegments) == 0 && len(c.Customer) > 0 {
+		c.SyncSegmentsFromCustomer()
+	}
+	if len(c.Customer) == 0 && len(c.CustomerSegments) > 0 {
+		c.SyncCustomerFromSegments()
+	}
+	return nil
+}
+
+func (c CustomerSegmentation) MarshalJSON() ([]byte, error) {
+	if len(c.Customer) == 0 && len(c.CustomerSegments) > 0 {
+		c.SyncCustomerFromSegments()
+	}
+	type customerSegmentation CustomerSegmentation
+	return json.Marshal(customerSegmentation(c))
 }
--- internal/service/customer_segmentation/core/helper.go
+++ internal/service/customer_segmentation/core/helper.go
@@ -1,30 +1,43 @@
 package core
 
 import (
-	// "gitlab.com/bersufekadgetachew/cbe-super-app-shared/shared/entities/model"
 	imodel "cbe-super-app-cps-action/internal/constants/model"
 )
 
 func MapCustomerSegmentationToMap(seg imodel.CustomerSegmentation, removedSegmentIds []string) map[string]interface{} {
-	subSegments := make([]map[string]interface{}, 0)
-	for _, sub := range seg.CustomerSegments {
-		subSegments = append(subSegments, map[string]interface{}{
-			"cus_sub_segment": sub.CustomerSubSegment,
-			"cust_group":      sub.CustomerGroup,
-			"cust_segment":    sub.CustomerSegment,
+	if len(seg.Customer) == 0 && len(seg.CustomerSegments) > 0 {
+		seg.SyncCustomerFromSegments()
+	}
+
+	customer := make([]map[string]interface{}, 0, len(seg.Customer))
+	for _, entry := range seg.Customer {
+		customer = append(customer, map[string]interface{}{
+			"group": map[string]interface{}{
+				"cust_group":       entry.Group.CustGroup,
+				"cust_group_label": entry.Group.CustGroupLabel,
+			},
+			"segment": map[string]interface{}{
+				"cust_segment_name":  entry.Segment.CustSegmentName,
+				"cust_segment_label": entry.Segment.CustSegmentLabel,
+			},
+			"sub_segment": map[string]interface{}{
+				"cust_sub_segment_name":  entry.SubSegment.CustSubSegmentName,
+				"cust_sub_segment_label": entry.SubSegment.CustSubSegmentLabel,
+			},
 		})
 	}
 
 	return map[string]interface{}{
 		"removed_customer_segment_ids": removedSegmentIds,
 		"customer_role": map[string]interface{}{
-			"id":   seg.CustomerRole.ID,
-			"name": seg.CustomerRole.Name,
+			"id":    seg.CustomerRole.ID,
+			"name":  seg.CustomerRole.Name,
+			"label": seg.CustomerRole.Label,
 		},
-		"t24_customer_sub_segments": subSegments,
-		"created_at":                seg.CreatedAt,
-		"is_enabled":                seg.IsEnabled,
-		"updated_at":                seg.UpdatedAt,
-		"is_deleted":                seg.IsDeleted,
+		"customer":     customer,
+		"created_at":   seg.CreatedAt,
+		"is_enabled":   seg.IsEnabled,
+		"updated_at":   seg.UpdatedAt,
+		"is_deleted":   seg.IsDeleted,
 	}
 }
