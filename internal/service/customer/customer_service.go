29c5a066496386eee6f377f52b15f165c3b3e4b5
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-05-20 16:20:23 +0300

    update on customer lookup

--- internal/service/customer/customer_service.go
+++ internal/service/customer/customer_service.go
@@ -535,6 +535,15 @@ func (d *customerService) GetCustomerDetailByID(ctx context.Context, id string) 
 		res.PersonalInfo.BranchName = coreRes[0].BranchName
 		res.PersonalInfo.Gender = coreRes[0].Gender
 		res.PersonalInfo.Email = coreRes[0].Email
+		res.PersonalInfo.Industry = coreRes[0].Industry
+		res.PersonalInfo.Ownership = coreRes[0].Ownership
+		res.PersonalInfo.PhoneNumber = coreRes[0].PhoneNo
+		res.PersonalInfo.Restriction = coreRes[0].Restriction
+		res.PersonalInfo.RestrictionType = coreRes[0].RestrictionType
+		res.PersonalInfo.Sector = coreRes[0].Sector
+		res.PersonalInfo.Target = coreRes[0].Target
+		res.PersonalInfo.CustomerSegment = coreRes[0].CustomerSegment
+		res.PersonalInfo.CustomerCategory = coreRes[0].Category
 	}
 	// for i, linkedAccount := range res.LinkedAccount {
 	// 	coreResBranch, err := d.core.LookupAccountByAccountNumberFromBps(ctx, linkedAccount.AccountNumber)
