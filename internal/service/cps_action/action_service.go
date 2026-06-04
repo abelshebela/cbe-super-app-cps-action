479a03d78121ca16437ab4dac218f0595cc27345
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-06-04 10:29:22 +0300

    log check on auditor

--- internal/service/cps_action/action_service.go
+++ internal/service/cps_action/action_service.go
@@ -850,6 +850,8 @@ func (ca *cpsActionService) GetCPSActionsForAuditor(ctx context.Context, userID 
 			log.Errorf("[CpsActionSvc][GetCPSActionsForAuditor] log filter err: %v", err)
 			return nil, "", err
 		}
+
+		log.Infof("[CpsActionSvc][GetCPSActionsForAuditor] log filter found %d matching action codes: %v", len(actionCodes), actionCodes)
 		filterParams.Filters["action_code"] = actionCodes
 	}
 
