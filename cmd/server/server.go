8b82617293c1ebf2f21c3228630e0bb46d73a049
Eyob Mamo Abiye <eyob.mamo@eaglelionsystems.com>
2026-06-05 21:28:56 +0300

    for build

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -45,6 +45,8 @@ func (s *HTTPServer) HTTPServerStop(ctx context.Context, log utils.Logger) {
 }
 
 
+
+
 func NewHTTPServer(config *config.VaultConfig, handler http.Handler) *HTTPServer {
 	srv := &http.Server{
 		Addr:         ":8080",
