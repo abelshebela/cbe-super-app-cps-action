ae03cfbcfc5d2cd9de03c0f10f3b8c9606ca7fca
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-06-08 20:25:25 +0300

    update action data with action code

--- internal/storage/persistance/user_action_log/repository.go
+++ internal/storage/persistance/user_action_log/repository.go
@@ -101,7 +101,7 @@ func (r *userActionLogRepository) GetActionCodesByActionLogFilter(ctx context.Co
 
 	log.Infof("[UserActionLog][GetActionCodesByActionLogFilter] filter=%+v", filter)
 
-	pipeline, err := buildActionCodeFilterPipeline(filter)
+	pipeline, err := r.buildActionCodeFilterPipeline(filter)
 	if err != nil {
 		log.Errorf("[UserActionLog][GetActionCodesByActionLogFilter] invalid filter: %v", err)
 		return nil, errors.New(localization.ErrorUnexpectedError.Code)
@@ -132,11 +132,12 @@ func (r *userActionLogRepository) GetActionCodesByActionLogFilter(ctx context.Co
 
 const fieldResponsibility = "$user_action_responsibilities"
 
-func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) (mongo.Pipeline, error) {
+func (r *userActionLogRepository) buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) (mongo.Pipeline, error) {
 	groupStage := bson.D{{Key: "_id", Value: "$action_code"}}
 	matchStage := bson.D{}
-
+	log := local_util.LoggerFromCtx(context.Background(), r.logger) // no need to pass real ctx since we won't log after this point
 	if len(filter.ActionStatuses) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying action status filter: %v", filter.ActionStatuses)
 		groupStage = append(groupStage, bson.E{
 			Key:   "action_status_match_count",
 			Value: sumWhen(bson.D{{Key: "$in", Value: bson.A{"$given_action_status", filter.ActionStatuses}}}),
@@ -145,6 +146,7 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 	}
 
 	if len(filter.AuditorStatuses) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying auditor status filter: %v", filter.AuditorStatuses)
 		if slices.Contains(filter.AuditorStatuses, "NOTCHECKED") {
 			// NOTCHECKED is represented as empty string in the DB, so we need to account for that in the count.
 			filter.AuditorStatuses = append(filter.AuditorStatuses, "")
@@ -157,6 +159,7 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 	}
 
 	if len(filter.ActionAuditorStatuses) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying action_auditor_status filter: %v", filter.ActionAuditorStatuses)
 		groupStage = append(groupStage, bson.E{
 			Key:   "action_auditor_status_match_count",
 			Value: sumWhen(bson.D{{Key: "$in", Value: bson.A{"$action_auditor_status", filter.ActionAuditorStatuses}}}),
@@ -169,6 +172,7 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 		return nil, fmt.Errorf("private users: %w", err)
 	}
 	if len(privateUserIDs) > 0 {
+
 		groupStage = append(groupStage, bson.E{
 			Key:   "private_user_match_count",
 			Value: sumWhen(bson.D{{Key: "$in", Value: bson.A{"$user_id", privateUserIDs}}}),
@@ -177,6 +181,7 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 	}
 
 	if len(filter.Levels) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying level filter: %v", filter.Levels)
 		groupStage = append(groupStage, bson.E{
 			Key: "level_match_count",
 			Value: sumWhen(bson.D{{Key: "$or", Value: bson.A{
@@ -188,6 +193,7 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 	}
 
 	if len(filter.Services) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying service filter: %v", filter.Services)
 		groupStage = append(groupStage, bson.E{
 			Key:   "service_match_count",
 			Value: sumWhen(bson.D{{Key: "$in", Value: bson.A{"$action_taken_service_name", filter.Services}}}),
@@ -200,6 +206,7 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 		return nil, fmt.Errorf("checker users: %w", err)
 	}
 	if len(checkerUserIDs) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying checker user filter: %v", filter.CheckerUserIDs)
 		groupStage = append(groupStage, bson.E{
 			Key: "checker_match_count",
 			Value: sumWhen(bson.D{{Key: "$and", Value: bson.A{
@@ -215,6 +222,8 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 		return nil, fmt.Errorf("auditor users: %w", err)
 	}
 	if len(auditorUserIDs) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying auditor user filter: %v", filter.AuditorUserIDs)
+
 		groupStage = append(groupStage, bson.E{
 			Key: "auditor_match_count",
 			Value: sumWhen(bson.D{{Key: "$and", Value: bson.A{
@@ -230,6 +239,8 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 		return nil, fmt.Errorf("maker users: %w", err)
 	}
 	if len(makerUserIDs) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying maker user filter: %v", filter.MakerUserIDs)
+
 		groupStage = append(groupStage, bson.E{
 			Key: "maker_match_count",
 			Value: sumWhen(bson.D{{Key: "$and", Value: bson.A{
@@ -241,6 +252,7 @@ func buildActionCodeFilterPipeline(filter imodel.UserActionLogActionCodeFilter) 
 	}
 
 	if len(filter.Responsibilities) > 0 {
+		log.Infof("[CPSAction][buildActionCodeFilterPipeline] applying responsibility filter: %v", filter.Responsibilities)
 
 		groupStage = append(groupStage, bson.E{
 			Key:   "responsibility_match_count",
