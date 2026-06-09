7a84ae26789e4058dc2b8d373cba6e43f228f10f
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-06-09 22:52:47 +0300

    use role delegation when returning user detail

--- internal/service/cps_user/cps_user_service.go
+++ internal/service/cps_user/cps_user_service.go
@@ -536,11 +536,21 @@ func (s *cpsUserService) GetCpsUserDetail(ctx context.Context, userCode string) 
 
 	var makerAlloc, checkerAlloc, auditorAlloc, portalCard, bpsCheckerAlloc, bpsAuditorAlloc []string
 	var roles *imodel.JobRole
-	if populated.JobTitle != "" {
-		roles, err = s.jobRoleRepo.FindByName(ctx, populated.JobTitle)
+	if populated.IsDelegationActive {
+		s.logger.Infof("fetching role by delegated role: %s", populated.DelegatedRole)
+		roles, err = s.jobRoleRepo.FindByRole(ctx, populated.DelegatedRole)
 		if err != nil {
-			span.AddEvent("failed to find role by name", trace.WithAttributes(attribute.String("error", err.Error())))
+			s.logger.Errorf("failed to find role by delegation err:%v", err)
+			span.AddEvent("failed to find role by delegation", trace.WithAttributes(attribute.String("error", err.Error())))
 			return nil, err
+		}
+	} else {
+		if populated.JobTitle != "" {
+			roles, err = s.jobRoleRepo.FindByName(ctx, populated.JobTitle)
+			if err != nil {
+				span.AddEvent("failed to find role by name", trace.WithAttributes(attribute.String("error", err.Error())))
+				return nil, err
+			}
 		}
 	}
 
