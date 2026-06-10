63ef2940a190d8524d0b6802c1139d25a5875426
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-06-10 11:02:25 +0300

    update bps action

--- internal/constants/dto/job_role/requrest.go
+++ internal/constants/dto/job_role/requrest.go
@@ -1,11 +1,13 @@
 package roles
 
 type RequestJobRolesCreate struct {
+	Code     string `json:"code" bson:"code"`
 	JobTitle string `json:"job_title" bson:"job_title"`
 	Role     string `json:"role" bson:"role"`
 }
 
 type RequestJobRolesUpdate struct {
+	Code     string `json:"code,omitempty" bson:"code"`
 	JobTitle string `json:"job_title" bson:"job_title"`
 	Role     string `json:"role" bson:"role"`
 }
