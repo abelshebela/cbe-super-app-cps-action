8fa0eab87ff974126815ba9e5515d1e42df01c0e
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-06-11 16:08:40 +0300

    resolve new core package issues

--- internal/constants/model/account_product_category.go
+++ internal/constants/model/account_product_category.go
@@ -3,14 +3,14 @@ package model
 import "time"
 
 type AccountProductCategory struct {
-	ID              string
-	AccountType     string
-	CategoryName    string
-	CBSCategoryCode string
-	Description     string
-	IsEnabled       bool
-	IsDeleted       bool
-	CreatedAt       time.Time
-	LastModifiedAt  time.Time
-	DeletedAt       *time.Time
+	ID              string     `bson:"id" json:"id"`
+	AccountType     string     `bson:"account_type" json:"account_type"`
+	CategoryName    string     `bson:"category_name" json:"category_name"`
+	CBSCategoryCode string     `bson:"cbs_category_code" json:"cbs_category_code"`
+	Description     string     `bson:"description" json:"description"`
+	IsEnabled       bool       `bson:"is_enabled" json:"is_enabled"`
+	IsDeleted       bool       `bson:"is_deleted" json:"is_deleted"`
+	CreatedAt       time.Time  `bson:"created_at" json:"created_at"`
+	LastModifiedAt  time.Time  `bson:"last_modified_at" json:"last_modified_at"`
+	DeletedAt       *time.Time `bson:"deleted_at" json:"deleted_at"`
 }
