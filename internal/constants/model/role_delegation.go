a95f591c09be2a79e63f9f2782469474cd7f136c
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-06-12 10:49:17 +0300

    feat: enhance RoleDelegation model with additional fields for department/branch names and role names

--- internal/constants/model/role_delegation.go
+++ internal/constants/model/role_delegation.go
@@ -9,17 +9,20 @@ import (
 type RoleDelegation struct {
 	ID bson.ObjectID `json:"id" bson:"_id,omitempty"`
 
-	DelegatedUserID                 string `json:"delegated_user_id" bson:"delegated_user_id"`
-	DelegatedUserUserCode           string `json:"delegated_user_user_code" bson:"delegated_user_user_code"`
-	DelegatedUserFullName           string `json:"delegated_user_full_name" bson:"delegated_user_full_name"`
-	DelegatedUserUserType           string `json:"delegated_user_user_type" bson:"delegated_user_user_type"`
-	DelegatedUserDepartmentOrBranch string `json:"delegated_user_department_or_branch" bson:"delegated_user_department_or_branch"`
-	DelegatedUserJobTitle           string `json:"delegated_user_job_title" bson:"delegated_user_job_title"`
-	DelegatedUserExistingRole       string `json:"delegated_user_existing_role" bson:"delegated_user_existing_role"`
-	DelegationType                  string `json:"delegation_type" bson:"delegation_type"`
-	DelegatedUserPhoneNumber        string `json:"delegated_user_phone_number" bson:"delegated_user_phone_number"`
-	DelegatedUserEmail              string `json:"delegated_user_email" bson:"delegated_user_email"`
+	DelegatedUserID                     string `json:"delegated_user_id" bson:"delegated_user_id"`
+	DelegatedUserUserCode               string `json:"delegated_user_user_code" bson:"delegated_user_user_code"`
+	DelegatedUserFullName               string `json:"delegated_user_full_name" bson:"delegated_user_full_name"`
+	DelegatedUserUserType               string `json:"delegated_user_user_type" bson:"delegated_user_user_type"`
+	DelegatedUserDepartmentOrBranch     string `json:"delegated_user_department_or_branch" bson:"delegated_user_department_or_branch"`
+	DelegatedUserDepartmentOrBranchName string `json:"delegated_user_department_or_branch_name" bson:"delegated_user_department_or_branch_name"`
+	DelegatedUserJobTitle               string `json:"delegated_user_job_title" bson:"delegated_user_job_title"`
+	DelegatedUserExistingRole           string `json:"delegated_user_existing_role" bson:"delegated_user_existing_role"`
+	DelegatedUserExistingRoleName       string `json:"delegated_user_existing_role_name" bson:"delegated_user_existing_role_name"`
+	DelegationType                      string `json:"delegation_type" bson:"delegation_type"`
+	DelegatedUserPhoneNumber            string `json:"delegated_user_phone_number" bson:"delegated_user_phone_number"`
+	DelegatedUserEmail                  string `json:"delegated_user_email" bson:"delegated_user_email"`
 
+	// taken as value with out modification
 	DelegatorUserID                 string `json:"delegator_user_id" bson:"delegator_user_id"`
 	DelegatorUserFullName           string `json:"delegator_user_full_name" bson:"delegator_user_full_name"`
 	DelegatorUserUserType           string `json:"delegator_user_user_type" bson:"delegator_user_user_type"`
@@ -27,8 +30,10 @@ type RoleDelegation struct {
 	DelegatorUserJobTitle           string `json:"delegator_user_job_title" bson:"delegator_user_job_title"`
 	DelegatorUserRole               string `json:"delegator_user_role" bson:"delegator_user_role"`
 
-	NewRoleID             string `json:"new_role_id" bson:"new_role_id"`
-	NewDepartmentOrBranch string `json:"new_department_or_branch" bson:"new_department_or_branch"`
+	NewRoleID                 string `json:"new_role_id" bson:"new_role_id"`
+	NewRoleIDName             string `json:"new_role_id_name" bson:"new_role_id_name"`
+	NewDepartmentOrBranch     string `json:"new_department_or_branch" bson:"new_department_or_branch"`
+	NewDepartmentOrBranchName string `json:"new_department_or_branch_name" bson:"new_department_or_branch_name"`
 
 	Enable                   bool      `json:"enable" bson:"enable"`
 	StartAt                  time.Time `json:"start_at" bson:"start_at"`
