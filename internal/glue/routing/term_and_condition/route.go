5265fa17f713cab82d7e290aaae41cb0950321f8
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-06-15 16:15:51 +0300

    enhance date validation and add logging in ParseUploadRequest

--- internal/glue/routing/term_and_condition/route.go
+++ internal/glue/routing/term_and_condition/route.go
@@ -17,9 +17,15 @@ const (
 func Init(router chi.Router, h tac_interface.TermAndConditionHandler, authMiddleware middleware.AuthMiddleware) {
 	auth := []func(next http.Handler) http.Handler{authMiddleware.AuthenticateToken}
 	routes := []glue.Route{
-		{Method: http.MethodGet, Path: pathCollection, Handler: h.GetAll, Middlewares: auth},
-		{Method: http.MethodGet, Path: pathResource, Handler: h.GetByID, Middlewares: auth},
-		{Method: http.MethodPost, Path: pathCollection, Handler: h.Upload, Middlewares: auth},
+		{
+			Method:      http.MethodGet,
+			Path:        pathCollection,
+			Handler:     h.GetAll,
+			Middlewares: auth},
+		{Method: http.MethodGet,
+			Path: pathResource, Handler: h.GetByID, Middlewares: auth},
+		{Method: http.MethodPost,
+			Path: pathCollection, Handler: h.Upload, Middlewares: auth},
 		{Method: http.MethodPut, Path: pathResource, Handler: h.Update, Middlewares: auth},
 		{Method: http.MethodDelete, Path: pathResource, Handler: h.Delete, Middlewares: auth},
 	}
