18e6540a53d5f669522f8ca0bd7b8760b4ee4a12
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-06-16 08:45:01 +0300

    update term and condistion

--- internal/constants/model/account_product.go
+++ internal/constants/model/account_product.go
@@ -10,6 +10,7 @@ type AccountProduct struct {
 	ProductLine           string     `bson:"product_line" json:"product_line"`
 	AccountCategoryID     string     `bson:"account_category_id" json:"account_category_id"`
 	CategoryName          string     `bson:"category_name" json:"category_name"`
+	CBSCategoryCode       string     `bson:"cbs_category_code" json:"cbs_category_code"`
 	AccountType           string     `bson:"-" json:"account_type"`
 	AccountCurrency       string     `bson:"account_currency" json:"account_currency"`
 	MinimumOpeningBalance float64    `bson:"minimum_opening_balance" json:"minimum_opening_balance"`
