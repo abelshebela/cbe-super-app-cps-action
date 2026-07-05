0dd1f169053ea7f2981d62ba191f5f79f8f8584c
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-07-05 18:14:49 +0300

    feat: add channel name extraction in SearchCustomerServiceLimitByCIF

--- internal/service/customer/customer_service.go
+++ internal/service/customer/customer_service.go
@@ -713,9 +713,9 @@ func (s *customerService) SearchCustomerServiceLimitByCIF(ctx context.Context, c
 
 	data := make([]customer_dto.CustomerServiceLimitResponses, 0)
 	indexByServiceCode := make(map[string]int)
-	channel_name := ""
 
 	res_cif := ""
+	channel_name := ""
 	serviceCode := ""
 	serviceName := ""
 	limit := ""
@@ -725,6 +725,9 @@ func (s *customerService) SearchCustomerServiceLimitByCIF(ctx context.Context, c
 		log.Infof("[CustomerSvc][SearchCustomerServiceLimitByCIF] detail: %v", detail)
 		if detail.CIF != "" {
 			res_cif = detail.CIF
+		}
+		if detail.Channel != "" {
+			channel_name = detail.Channel
 		}
 		if detail.ServiceCode != "" {
 			serviceCode = detail.ServiceCode
