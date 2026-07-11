660662f6c52197e1c22f0e64740e1636abca42b1
Daniel Asfaw <daniel.asfaw@eaglelionsystems.com>
2026-07-11 11:22:19 +0300

    fix: correct sql syntax of scan parameters in FindAllForBlockParents method

--- internal/storage/persistance/access_list_segmentation/oracle/implementaion.go
+++ internal/storage/persistance/access_list_segmentation/oracle/implementaion.go
@@ -302,7 +302,7 @@ func (q *accessListSegmentationOracle) FindAllForBlockParents(ctx context.Contex
 	err := q.db.QueryRowContext(ctx, `
     SELECT federal_region_name, district_name
     FROM COMPANY
-    WHERE or district_name = :1 or federal_region_name = :1
+    WHERE district_name = :1 or federal_region_name = :1
 `, geographicalID).Scan(&districtName, &federalRegionName)
 	if err != nil {
 		log.Errorf("[AccessListSegmentation][FindAllForBlockParents] failed to fetch block: %v", err)
