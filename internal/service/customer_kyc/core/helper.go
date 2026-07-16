acb751753baf173b6faf56cd3b0a79a35c40b2de
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-07-16 11:37:19 +0300

    self_activation_kyc sms message get updated

--- internal/service/customer_kyc/core/helper.go
+++ internal/service/customer_kyc/core/helper.go
@@ -250,8 +250,11 @@ func MapSelfActivationUserToResponse(u *imodel.SelfActivationUser) *dto.Customer
 		KYCStatus:           u.KYC.KYCStatus,
 		MoneyLaunderingFree: nil,
 		TermsAndConditions:  "",
-		CreatedAt:           u.CreatedAt.Format(time.RFC3339),
-		UpdatedAt:           u.LastModifiedAt.Format(time.RFC3339),
+		// KYCReviewStartedAt:  u.sta,
+		// KYCReviewExpiresAt: u.ExpiryDate,
+		// Reviewer: u.
+		CreatedAt: u.CreatedAt.Format(time.RFC3339),
+		UpdatedAt: u.LastModifiedAt.Format(time.RFC3339),
 	}
 
 	return &response
