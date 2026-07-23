9154b2a436a9af1f0a84d23c0e9c7f2b334e64b4
Eyob Mamo Abiye <eyob.mamo@eaglelionsystems.com>
2026-07-23 12:32:07 +0300

    fix on account product issue related to interest rate

--- internal/storage/persistance/account_product/oracle/repository.go
+++ internal/storage/persistance/account_product/oracle/repository.go
@@ -66,8 +66,8 @@ func (r *repository) createInner(ctx context.Context, ap *imodel.AccountProduct)
 		 ACCOUNT_CATEGORY_ID, ACCOUNT_CURRENCY,
 		 MINIMUM_OPENING_BALANCE, MINIMUM_MAINTENANCE_FEE, INTEREST_FEE,
 		 FAQ_URL, PRODUCT_FEATURES, HAS_PHYSICAL_CARD, HAS_VIRTUAL_CARD,
-		 PRODUCT_ICON, PRODUCT_COVER_IMAGE, IS_ENABLED, IS_DELETED,IS_AVAILABE_FOR_ONBORDING,INTEREST_RATE)
-		VALUES (:1, :2, :3, HEXTORAW(:4), :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16,:17,:18)`
+		 PRODUCT_ICON, PRODUCT_COVER_IMAGE, IS_ENABLED, IS_DELETED,IS_AVAILABE_FOR_ONBORDING)
+		VALUES (:1, :2, :3, HEXTORAW(:4), :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16,:17)`
 
 	if _, err := r.db.ExecContext(ctx, q,
 		ap.CBSProductCode,
@@ -87,7 +87,7 @@ func (r *repository) createInner(ctx context.Context, ap *imodel.AccountProduct)
 		ap_core.BoolToInt(ap.IsEnabled),
 		ap_core.BoolToInt(ap.IsDeleted),
 		ap_core.BoolToInt(ap.IsAvailableForOnbording),
-		ap.InterestRate,
+		// ap.InterestRate,
 	); err != nil {
 		log.Errorf("[APOracle][Create] insert: %v", err)
 		return local_util.HandleDBError(err)
@@ -105,8 +105,8 @@ func (r *repository) Update(ctx context.Context, id string, ap *imodel.AccountPr
 		    MINIMUM_OPENING_BALANCE = :6, MINIMUM_MAINTENANCE_FEE = :7, INTEREST_FEE = :8,
 		    FAQ_URL = :9, PRODUCT_FEATURES = :10, HAS_PHYSICAL_CARD = :11,
 		    HAS_VIRTUAL_CARD = :12, PRODUCT_ICON = :13, PRODUCT_COVER_IMAGE = :14,
-		    LAST_MODIFIED_AT = :15,IS_AVAILABE_FOR_ONBORDING = :16,INTEREST_RATE = :17
-		WHERE ID = HEXTORAW(:18) AND IS_DELETED = 0`
+		    LAST_MODIFIED_AT = :15,IS_AVAILABE_FOR_ONBORDING = :16
+		WHERE ID = HEXTORAW(:17) AND IS_DELETED = 0`
 
 	res, err := r.db.ExecContext(ctx, q,
 		ap.CBSProductCode,
@@ -125,7 +125,7 @@ func (r *repository) Update(ctx context.Context, id string, ap *imodel.AccountPr
 		ap_core.NullStringFromString(ap.ProductCoverImage),
 		time.Now(),
 		ap_core.BoolToInt(ap.IsAvailableForOnbording),
-		ap.InterestRate,
+		// ap.InterestRate,
 		id,
 	)
 	if err != nil {
