e40e5ba0dba6d86c8390a75b90173da028c9ada5
Bantamlak <bantamlak.tilahun@eaglelionsystems.com>
2026-07-28 17:31:43 +0300

    fix: query params are ready for use

--- internal/constants/localization/response_codes.go
+++ internal/constants/localization/response_codes.go
@@ -681,6 +681,8 @@ var ResponseCodesList = []ResponseCode{
 	CustomerDisabledSuccessfully,
 	CustomerEnableRequestCreatedSuccessfully,
 	ErrorCustomerAlreadyDisabled,
+	ErrorCustomerAlreadyBarred,
+	ErrorCustomerAlreadyUnbarred,
 	ErrorCustomerAlreadyEnabled,
 	ErrorCustomerNotFound,
 	ErrorIdNotSetOnQueryParam,
@@ -9090,6 +9092,20 @@ var (
 		Code:       "ERROR_CUSTOMER_ALREADY_DISABLED",
 		StatusCode: StatusConflict,
 		Message:    "Customer is already disabled",
+		Type:       "error",
+	}
+
+	ErrorCustomerAlreadyBarred = ResponseCode{
+		Code:       "ERROR_CUSTOMER_ALREADY_BARRED",
+		StatusCode: StatusConflict,
+		Message:    "Customer is already barred",
+		Type:       "error",
+	}
+
+	ErrorCustomerAlreadyUnbarred = ResponseCode{
+		Code:       "ERROR_CUSTOMER_ALREADY_UNBARRED",
+		StatusCode: StatusConflict,
+		Message:    "Customer is already unbarred",
 		Type:       "error",
 	}
 
