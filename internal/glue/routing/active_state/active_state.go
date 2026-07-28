5be47703a77998a54d9850913e60271a44c70d28
dawit1254 <dawit.girma@eaglelionsystems.com>
2026-07-28 12:15:39 +0300

    update new change on the active state update

---
+++ internal/glue/routing/active_state/active_state.go
@@ -0,0 +1,24 @@
+package active_state
+
+import (
+	activeState "cbe-super-app-cps-action/internal/constants/interfaces/active_state"
+	"cbe-super-app-cps-action/internal/glue"
+	"cbe-super-app-cps-action/internal/handlers/middleware"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Init(router chi.Router, handler activeState.ActiveState, authMiddleware middleware.AuthMiddleware) {
+	routes := []glue.Route{
+		{
+			Method:  http.MethodGet,
+			Path:    "/active_state",
+			Handler: handler.UpdateStatus,
+			Middlewares: []func(next http.Handler) http.Handler{
+				authMiddleware.AuthenticateToken,
+			},
+		},
+	}
+	glue.RegisterRoutes(router, routes)
+}
